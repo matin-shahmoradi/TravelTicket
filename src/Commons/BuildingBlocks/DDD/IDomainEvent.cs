0b30f47493dfe15ebd234797c05cfbb5e47b9725
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-04-25 19:49:30 +0330

    Add DDD abstractions.

---
+++ src/Commons/BuildingBlocks/DDD/IDomainEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace BuildingBlocks.DDD
+{
+    public interface IDomainEvent : INotification
+    {
+        Guid EventId => Guid.NewGuid();
+        public DateTime OccurredOn => DateTime.UtcNow;
+        public string eventType => GetType().AssemblyQualifiedName!;
+    }
+}
