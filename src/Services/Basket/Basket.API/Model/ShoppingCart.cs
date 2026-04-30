f1c8940f262aa9f75a2ba69639314ce111730672
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-04-30 14:56:20 +0330

    refactor basket domain based on ddd.

--- src/Services/Basket/Basket.API/Model/ShoppingCart.cs
+++ src/Services/Basket/Basket.API/Model/ShoppingCart.cs
@@ -1,19 +1,50 @@
-﻿namespace Basket.API.Model
-{
-    public class ShoppingCart
-    {  
-        public Guid Id { get; set; } = Guid.NewGuid();
-        public List<ShoppingCartItem> Items { get; set; } = new();
-        public decimal TotalPrice => Items.Sum(x => x.Price * x.Quantity);
-        public string TravlerNumber { get; set; } = default!;
+﻿using Basket.API.StrongIdTypes;
+using BuildingBlocks.DDD;
 
-        public ShoppingCart()
+namespace Basket.API.Model
+{
+    public class ShoppingCart : Aggregate<ShoppingCartId>
+    {
+        public string Username { get; private set; } = default!;
+        private readonly List<ShoppingCartItem> _items = new();
+        public IReadOnlyList<ShoppingCartItem> Items => _items.AsReadOnly();
+        public decimal TotalPrice => Items.Sum(x => x.Price * x.Quantity);
+
+        public static ShoppingCart Create(Guid id, string username)
         {
-            
+            ArgumentNullException.ThrowIfNull(username);
+
+            var cart = new ShoppingCart
+            {
+                Id = ShoppingCartId.New(id),
+                Username = username,
+            };
+            return cart;
         }
-        public ShoppingCart(string travlerNumber)
+
+        public void AddItem(Guid ticketId, int quantity, decimal price)
         {
-            TravlerNumber = travlerNumber;
+            ArgumentOutOfRangeException.ThrowIfNegative(quantity);
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+            var existingItem = Items.FirstOrDefault(x => x.TicketId == ticketId);
+            if (existingItem != null)
+            {
+                existingItem.Quantity += quantity;
+            }
+            else
+            {
+                var newItem = new ShoppingCartItem(Id,ticketId, quantity, price);
+                _items.Add(newItem);
+            }
+        }
+        public void RemoveItem(Guid ticketId)
+        {
+            var existingItem = Items.FirstOrDefault(x => x.TicketId == ticketId);
+            if(existingItem != null)
+            {
+                _items.Remove(existingItem);
+            }
         }
     }
 }
--- src/Services/Basket/Basket.API/Model/ShoppingCartItem.cs
+++ src/Services/Basket/Basket.API/Model/ShoppingCartItem.cs
@@ -1,9 +1,21 @@
-﻿namespace Basket.API.Model
+﻿using Basket.API.StrongIdTypes;
+using BuildingBlocks.DDD;
+
+namespace Basket.API.Model
 {
-    public class ShoppingCartItem
+    public class ShoppingCartItem : Entity<Guid>
     {
-        public int Quantity { get; set; }
-        public decimal Price { get; set; }
-        public Guid TicketId { get; set; }
+        public ShoppingCartId ShoppingCartId { get; private set; }
+        public Guid TicketId { get; private set; }
+        public int Quantity { get; internal set; }
+        public decimal Price { get; private set; }
+
+        internal ShoppingCartItem(ShoppingCartId shoppingCartId, Guid ticketId, int quantity, decimal price)
+        {
+            ShoppingCartId = shoppingCartId;
+            TicketId = ticketId;
+            Quantity = quantity;
+            Price = price;
+        }
     }
 }
