9fcd0a86e771705d793f36d46a31d8861256107d
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-04-30 14:57:02 +0330

    add strongly type id for shopping cart id.

---
+++ src/Services/Basket/Basket.API/StrongIdTypes/ShoppingCartId.cs
@@ -0,0 +1,8 @@
+﻿namespace Basket.API.StrongIdTypes
+{
+    [StronglyTypedId]
+    public partial struct ShoppingCartId()
+    {
+        public static ShoppingCartId New(Guid Value) => new ShoppingCartId(Value);
+    }
+}
