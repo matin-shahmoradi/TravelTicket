09157ce80a2b50182101913b8b95b83ce7b4c43f
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-05-03 10:51:34 +0330

    move dependencies to extionsion class

---
+++ src/Services/Basket/Basket.API/BasketExtensions/BasketExtension.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Basket.API.BasketExtensions
+{
+    public static class BasketExtension
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services , IConfiguration configuration)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            services.AddMediatR(config =>
+            {
+                config.RegisterServicesFromAssembly(assembly);
+                config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+                config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+            });
+
+            services.AddDbContext<BacketDbContext>(cfg =>
+            {
+                cfg.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            services.AddStackExchangeRedisCache(config =>
+            {
+                config.Configuration = configuration.GetConnectionString("Redis");
+                config.InstanceName = "BasketRedis";
+            });
+
+            services.AddScoped<IBasketRepository, BasketRepository>();
+            services.Decorate<IBasketRepository, CachedBasketRepository>();
+
+            services.AddCarter();
+            services.AddValidatorsFromAssembly(assembly);
+            services.AddSwaggerGen();
+            services.AddProblemDetails();
+
+            return services;
+        }
+
+        public static async Task MigrateDatabase(this WebApplication app)
+        {
+            using var scoped = app.Services.CreateScope();
+            var context = scoped.ServiceProvider.GetRequiredService<BacketDbContext>();
+
+            await context.Database.MigrateAsync();
+        }
+    }
+}
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,40 +1,18 @@
+using Basket.API.BasketExtensions;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 var assembly = typeof(Program).Assembly;
 // Add services to the container.
 
 builder.Services.AddControllers();
-
-builder.Services.AddMediatR(config =>
-{
-    config.RegisterServicesFromAssembly(assembly);
-    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
-    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
-});
-builder.Services.AddMarten(config =>
-{
-    config.Connection(builder.Configuration.GetConnectionString("DefaultConnection")!);
-    config.Schema.For<ShoppingCart>().Identity(x => x.TravlerNumber);
-}).UseLightweightSessions();
-builder.Services.AddStackExchangeRedisCache(config =>
-{
-    config.Configuration = builder.Configuration.GetConnectionString("Redis");
-    config.InstanceName = "BasketRedis";
-});
-
-builder.Services.AddScoped<IBasketRepository,BasketRepository>();
-builder.Services.Decorate<IBasketRepository,CachedBasketRepository>();
-
-builder.Services.AddCarter();
-builder.Services.AddValidatorsFromAssembly(assembly);
-builder.Services.AddSwaggerGen();
-builder.Services.AddProblemDetails();
-
+builder.Services.AddServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.MigrateDatabase().GetAwaiter().GetResult();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
