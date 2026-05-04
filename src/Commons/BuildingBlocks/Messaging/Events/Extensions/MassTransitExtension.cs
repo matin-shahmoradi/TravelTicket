06a79b24954c1fdcc3794f067555f7d76ed9bb6e
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-05-04 14:18:30 +0330

    add mass transit configuration.

---
+++ src/Commons/BuildingBlocks/Messaging/Events/Extensions/MassTransitExtension.cs
@@ -0,0 +1,33 @@
+﻿using MassTransit;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace BuildingBlocks.Messaging.Events.Extensions
+{
+    public static class MassTransitExtension
+    {
+        public static IServiceCollection AddMassTransitWithAssembly(this IServiceCollection services , Assembly assembly)
+        {
+            services.AddMassTransit(cfg =>
+            {
+                cfg.SetKebabCaseEndpointNameFormatter();
+
+                cfg.SetInMemorySagaRepositoryProvider();
+
+                cfg.AddConsumers(assembly);
+
+                cfg.AddSagaStateMachines(assembly);
+
+                cfg.AddSagas(assembly);
+
+                cfg.AddActivities(assembly);
+
+                cfg.UsingInMemory((context, configurator) =>
+                {
+                    configurator.ConfigureEndpoints(context);
+                });
+            });
+            return services;
+        }
+    }
+}
