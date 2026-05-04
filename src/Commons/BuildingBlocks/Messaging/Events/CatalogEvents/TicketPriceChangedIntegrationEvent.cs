9984d6e9732a4cf2d6df68c96b5d50ac81a3ecef
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-05-04 14:19:42 +0330

    add ticket price changed integration event class.

---
+++ src/Commons/BuildingBlocks/Messaging/Events/CatalogEvents/TicketPriceChangedIntegrationEvent.cs
@@ -0,0 +1,12 @@
+﻿namespace BuildingBlocks.Messaging.Events.CatalogEvents
+{
+    public class TicketPriceChangedIntegrationEvent : IntegrationEvent
+    {
+        public Guid TicketId { get; set; } 
+        public string Origin { get; set; } = default!;
+        public string Destination { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public DateTime TravelDate { get; set; }
+        public decimal Price { get; set; }
+    }
+}
