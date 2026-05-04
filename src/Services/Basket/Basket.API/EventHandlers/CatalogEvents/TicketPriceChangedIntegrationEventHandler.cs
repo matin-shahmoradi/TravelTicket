a1d7b95baa3edcf22d80c2dc4a58038708908623
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-05-04 14:22:17 +0330

    add ticket price changed event handlers.

---
+++ src/Services/Basket/Basket.API/EventHandlers/CatalogEvents/TicketPriceChangedIntegrationEventHandler.cs
@@ -0,0 +1,19 @@
+﻿using BuildingBlocks.Messaging.Events.CatalogEvents;
+using MassTransit;
+
+namespace Basket.API.EventHandlers.CatalogEvents
+{
+    public class TicketPriceChangedIntegrationEventHandler(
+        ISender sender,
+        Logger<TicketPriceChangedIntegrationEventHandler> logger) 
+        :
+        IConsumer<TicketPriceChangedIntegrationEvent>
+    {
+        public Task Consume(ConsumeContext<TicketPriceChangedIntegrationEvent> context)
+        {
+            logger.LogInformation("Integration Event Handled: {IntegrationEvent}", context.Message.GetType().Name);
+            
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/EventHandlers/TicketPriceChangedEventHandler.cs
@@ -0,0 +1,25 @@
+﻿using BuildingBlocks.Messaging.Events.CatalogEvents;
+using MassTransit;
+
+namespace Catalog.API.EventHandlers
+{
+    public class TicketPriceChangedEventHandler
+        (IBus bus,ILogger<TicketPriceChangedEventHandler> logger)
+        : INotificationHandler<TicketPriceChangedEvent>
+    {
+        public async Task Handle(TicketPriceChangedEvent notification, CancellationToken cancellationToken)
+        {
+            var ticketPriceChangedIntegrationEvent = new TicketPriceChangedIntegrationEvent
+            {
+                TicketId = notification.ticket.Id.Value,
+                Origin = notification.ticket.Origin,
+                Destination = notification.ticket.Destination,
+                Description = notification.ticket.Description,
+                TravelDate = notification.ticket.TravelDate,
+                Price = notification.ticket.Price,
+            };
+
+           await bus.Publish(ticketPriceChangedIntegrationEvent , cancellationToken);
+        }
+    }
+}
