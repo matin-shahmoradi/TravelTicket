868ba81da8bdb711697ee764e878a986aeb91013
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-05-15 23:30:31 +0330

    Implement add item to basket endpoint.

---
+++ src/Services/Basket/Basket.API/Basket/AddItemToBasket/AddItemToBasketEndpoint.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.API.Basket.AddItemToBasket
+{
+    public class AddItemToBasketEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("baskets/{username}", async (
+                HttpContext context,
+                ISender sender, 
+                [FromBody] AddItemToBasketDto dto) =>
+            {
+                var command = new AddItemToBasketCommand(dto);
+
+                var addItemToBasket = await sender.Send(command);
+
+                if (!addItemToBasket.IsSuccess)
+                {
+                    return addItemToBasket.ToHttpResult(context);
+                }
+                // TODO : Redirect user to GetBasket Endpoint.
+                return Results.Ok(addItemToBasket);
+            });
+        }
+    }
+}
