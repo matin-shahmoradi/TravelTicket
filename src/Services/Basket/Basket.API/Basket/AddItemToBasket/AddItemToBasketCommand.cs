a19afd07bb10423324298344824941b56f5a9e0c
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-05-15 23:28:25 +0330

    implement add item to basket command.

---
+++ src/Services/Basket/Basket.API/Basket/AddItemToBasket/AddItemToBasketCommand.cs
@@ -0,0 +1,21 @@
+﻿namespace Basket.API.Basket.AddItemToBasket
+{
+    public record AddItemToBasketCommand(AddItemToBasketDto AddItemToBasketDto) : ICommand<Result<ShoppingCartDto>>
+    {
+    }
+
+    public class AddItemToBasketCommandValidator : AbstractValidator<AddItemToBasketCommand> 
+    {
+        public AddItemToBasketCommandValidator()
+        {
+            RuleFor(x => x.AddItemToBasketDto.Username)
+                .NotEmpty().WithMessage("username cant be empty.");
+
+            RuleFor(x => x.AddItemToBasketDto.Quantity)
+                .GreaterThan(0).WithMessage("Quantity should be greater than zero");
+
+            RuleFor(x => x.AddItemToBasketDto.TicketId)
+                .NotEmpty().WithMessage("ticket id cant be empty.");
+        }
+    }
+}
---
+++ src/Services/Basket/Basket.API/Common/Dtos/AddItemToBasketDto.cs
@@ -0,0 +1,4 @@
+﻿namespace Basket.API.Common.Dtos
+{
+    public record AddItemToBasketDto(Guid TicketId, string Username, int Quantity);
+}
