88b13d0126ce5d8fa780f74c82a3bf44841ad506
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-05-21 20:48:20 +0330

    feat(auth): configure ef core and database connection

---
+++ src/Services/Auth/AuthService/Program.cs
@@ -0,0 +1,40 @@
+using AuthService.Data;
+using AuthService.Model;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<AuthDbContext>(cfg =>
+{
+    cfg.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>(cfg =>
+{
+    cfg.Password.RequiredLength = 8;
+})
+    .AddEntityFrameworkStores<AuthDbContext>()
+    .AddDefaultTokenProviders();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
