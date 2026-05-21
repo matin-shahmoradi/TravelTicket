d353c01d6edd9f5ffe25067f382769df442fa46c
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-05-21 20:45:21 +0330

    feat(auth): add auth entity and db context

---
+++ src/Services/Auth/AuthService/Data/AuthDbContext.cs
@@ -0,0 +1,14 @@
+﻿using AuthService.Model;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthService.Data
+{
+    public class AuthDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
