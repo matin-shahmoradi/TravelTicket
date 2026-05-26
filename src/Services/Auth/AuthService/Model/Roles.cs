0868250c08ec9cb1cee972273fdc578f3f36f9dd
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-05-26 18:23:42 +0330

    feat(Auth) : add roles for seed identity database.

---
+++ src/Services/Auth/AuthService/Model/Roles.cs
@@ -0,0 +1,9 @@
+﻿namespace AuthService.Model
+{
+    public class Roles
+    {
+        public const string User = "User";
+        public const string Admin = "Admin";
+        public const string SuperAdmin = "SuperAdmin";
+    }
+}
