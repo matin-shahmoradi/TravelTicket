7058089599d8a6b88e8ec1a9fd2fa29ac34e7dfb
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-06-04 02:28:52 +0330

    feat(Auth): add UserResponseDto to return user data

---
+++ src/Services/Auth/AuthService/Model/DTOs/UserDtos/UserResponseDto.cs
@@ -0,0 +1,12 @@
+﻿namespace AuthService.Model.DTOs.UserDtos
+{
+    public sealed class UserResponseDto
+    {
+        public string Id { get; init; } = default!;
+        public string? UserName { get; init; }
+        public string Email { get; init; } = default!;
+        public string? PhoneNumber { get; init; }
+        public string? FirstName { get; init; }
+        public string? LastName { get; init; }
+    }
+}
