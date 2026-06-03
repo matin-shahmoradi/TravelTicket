9db0d4fbdbad16186915a468618d3bffbda3b9b4
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-06-03 20:22:51 +0330

    feat(Auth): Add user password validation service interface and impl
    Introduced IUserSignInManagerService interface for user password validation. Implemented UserSignInManagerService using ASP.NET Core SignInManager to check user credentials and return a boolean result.

---
+++ src/Services/Auth/AuthService/Interfaces/IUserSignInManagerService.cs
@@ -0,0 +1,9 @@
+﻿using AuthService.Model;
+
+namespace AuthService.Interfaces
+{
+    public interface IUserSignInManagerService
+    {
+        Task<bool> ValidateUserPassword(ApplicationUser user, string password);
+    }
+}
---
+++ src/Services/Auth/AuthService/Repositories/UserSignInManagerService.cs
@@ -0,0 +1,15 @@
+﻿using AuthService.Interfaces;
+using AuthService.Model;
+using Microsoft.AspNetCore.Identity;
+
+namespace AuthService.Repositories
+{
+    public class UserSignInManagerService(SignInManager<ApplicationUser> signInManager) : IUserSignInManagerService
+    {
+        public async Task<bool> ValidateUserPassword(ApplicationUser user, string password)
+        {
+            var signInResult = await signInManager.CheckPasswordSignInAsync(user, password, false);
+            return signInResult.Succeeded ? true : false;
+        }
+    }
+}
