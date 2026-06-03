88869e78fd73d2cbd3df470e542ec0ab6bb5739c
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-06-03 20:17:31 +0330

    feat(Auth): Add user management command service interface
    Introduced IUserManagerCommandService interface for user creation, role assignment, and email verification token generation. Added UserManagerCommandService implementation using ASP.NET Core Identity's UserManager.

---
+++ src/Services/Auth/AuthService/Interfaces/IUserManagerCommandService.cs
@@ -0,0 +1,12 @@
+﻿using AuthService.Model;
+using Microsoft.AspNetCore.Identity;
+
+namespace AuthService.Interfaces
+{
+    public interface IUserManagerCommandService
+    {
+        Task<IdentityResult> CreateUserAsync(ApplicationUser user, string password);
+        Task<IdentityResult> AddUserToRoleAsync(ApplicationUser user, string role);
+        Task<string> CreateEmailVerificationToken(ApplicationUser user);
+    }
+}
---
+++ src/Services/Auth/AuthService/Repositories/UserManagerCommandService.cs
@@ -0,0 +1,18 @@
+﻿using AuthService.Interfaces;
+using AuthService.Model;
+using Microsoft.AspNetCore.Identity;
+
+namespace AuthService.Repositories
+{
+    public sealed class UserManagerCommandService(UserManager<ApplicationUser> userManager) : IUserManagerCommandService
+    {
+        public async Task<IdentityResult> CreateUserAsync(ApplicationUser user, string password) =>
+            await userManager.CreateAsync(user, password);
+
+        public async Task<IdentityResult> AddUserToRoleAsync(ApplicationUser user, string role) =>
+            await userManager.AddToRoleAsync(user, role);
+
+        public async Task<string> CreateEmailVerificationToken(ApplicationUser user) =>
+            await userManager.GenerateEmailConfirmationTokenAsync(user);
+    }
+}
