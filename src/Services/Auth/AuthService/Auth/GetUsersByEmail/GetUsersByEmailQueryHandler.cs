5bc16e4b93f8f3d8121871f99d8f5e8b5c7bc2ca
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-06-03 20:14:06 +0330

    refactor(Auth): GetUsersByEmailQueryHandler for new service API
    Changed handler to internal sealed,
     updated to use IUserManagerQueryService and AutoMapper
    , and switched to GetUserByEmailAsync.
     Now maps user entity to UserResponseDto before returning.

--- src/Services/Auth/AuthService/Auth/GetUsersByEmail/GetUsersByEmailQueryHandler.cs
+++ src/Services/Auth/AuthService/Auth/GetUsersByEmail/GetUsersByEmailQueryHandler.cs
@@ -1,22 +1,25 @@
 ﻿using AuthService.Interfaces;
 using AuthService.Model.DTOs.UserDtos;
+using AutoMapper;
 using BuildingBlocks;
 using BuildingBlocks.CQRS;
 
 namespace AuthService.Auth.GetUsersByEmail
 {
-    public class GetUsersByEmailQueryHandler(IUserQueryService userQueryService)
+    internal sealed class GetUsersByEmailQueryHandler(IUserManagerQueryService userQueryService, IMapper mapper)
         : IQueryHandler<GetUserByEmailQuery, Result<UserResponseDto>>
     {
         public async Task<Result<UserResponseDto>> Handle(GetUserByEmailQuery query, CancellationToken cancellationToken)
         {
-            var getUser = await userQueryService.GetUsersByEmailAsync(query.Email, cancellationToken);
+            var getUser = await userQueryService.GetUserByEmailAsync(query.Email, cancellationToken);
 
             if (getUser == null)
                 return Result<UserResponseDto>.Failure(
-                    Error.NotFoundError(message:$"User with email {query.Email} is not exist"));
+                    Error.NotFoundError(message: $"User with email {query.Email} is not exist"));
 
-            return Result<UserResponseDto>.Success(getUser);
+            var userResponseDto = mapper.Map<UserResponseDto>(getUser);
+
+            return Result<UserResponseDto>.Success(userResponseDto);
         }
     }
 }
