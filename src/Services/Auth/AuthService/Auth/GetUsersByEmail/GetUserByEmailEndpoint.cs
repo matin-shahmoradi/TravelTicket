ca8b82b0a8a995c05395d4df137c7325ebd63514
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-06-03 20:42:15 +0330

    feat(Auth): add GetUserByEmailEndpoint for user lookup by email
    Introduced GetUserByEmailEndpoint in the AuthService.Auth.GetUsersByEmail namespace. This endpoint handles GET requests to "auth/user/{email}", sends a GetUserByEmailQuery via MediatR, and returns the result as an HTTP response.

---
+++ src/Services/Auth/AuthService/Auth/GetUsersByEmail/GetUserByEmailEndpoint.cs
@@ -0,0 +1,27 @@
+﻿using BuildingBlocks.Extensions;
+using Carter;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthService.Auth.GetUsersByEmail
+{
+    public class GetUserByEmailEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("auth/user/{email}" , async (
+                ISender sender ,
+                HttpContext httpContext,
+                [FromRoute] string email) =>
+            {
+                var query = new GetUserByEmailQuery(email);
+                var result = await sender.Send(query);
+
+                if (!result.IsSuccess)
+                    return result.ToHttpResult(httpContext);
+
+                return Results.Ok(result);
+            });
+        }
+    }
+}
