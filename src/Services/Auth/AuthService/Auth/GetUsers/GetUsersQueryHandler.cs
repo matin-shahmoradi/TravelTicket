7f0792cf79cb49cf5d3926ac63bcd561216ba21a
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-06-03 20:45:26 +0330

    refactor(Auth): make ConfirmEmailCommandHandler internal and sealed and map ApplicationUser instance to UserResponseDto

--- src/Services/Auth/AuthService/Auth/GetUsers/GetUsersQueryHandler.cs
+++ src/Services/Auth/AuthService/Auth/GetUsers/GetUsersQueryHandler.cs
@@ -1,21 +1,23 @@
 ﻿using AuthService.Interfaces;
 using AuthService.Model.DTOs.UserDtos;
+using AutoMapper;
 using BuildingBlocks;
 using BuildingBlocks.CQRS;
 
 namespace AuthService.Auth.GetUsers
 {
     public record GetUsersQuery : IQuery<Result<IEnumerable<UserResponseDto>>>;
-    public class GetUsersQueryHandler(IUserQueryService userQueryService) 
+    internal sealed class GetUsersQueryHandler(IUserManagerQueryService userQueryService, IMapper mapper)
         : IQueryHandler<GetUsersQuery, Result<IEnumerable<UserResponseDto>>>
     {
         public async Task<Result<IEnumerable<UserResponseDto>>> Handle(GetUsersQuery query, CancellationToken cancellationToken)
         {
             var users = await userQueryService.GetUsersAsync(cancellationToken);
             if (users is null)
-                return Result<IEnumerable<UserResponseDto>>.Failure(Error.NotFoundError(message:"Users not Found!"));
+                return Result<IEnumerable<UserResponseDto>>.Failure(Error.NotFoundError(message: "Users not Found!"));
 
-            return Result<IEnumerable<UserResponseDto>>.Success(users);
+            var usersDto = mapper.Map<List<UserResponseDto>>(users);
+            return Result<IEnumerable<UserResponseDto>>.Success(usersDto);
         }
     }
 }
