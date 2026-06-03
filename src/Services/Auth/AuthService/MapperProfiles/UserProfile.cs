79c77bacb1623a567643a9f61571890d0416be1d
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-06-03 20:39:45 +0330

    feat(Auth) : create map between ApplicationUser and UserResponseDto

---
+++ src/Services/Auth/AuthService/MapperProfiles/UserProfile.cs
@@ -0,0 +1,21 @@
+﻿using AuthService.Model;
+using AuthService.Model.DTOs.UserDtos;
+using AutoMapper;
+
+namespace AuthService.MapperProfiles
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<ApplicationUser, UserResponseDto>()
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(x => x.Id))
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(x => x.Email))
+                .ForMember(dest => dest.UserName, opt => opt.MapFrom(x => x.UserName))
+                .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(x => x.PhoneNumber))
+                .ForMember(dest => dest.FirstName, opt => opt.MapFrom(x => x.FirstName))
+                .ForMember(dest => dest.LastName, opt => opt.MapFrom(x => x.LastName))
+                .ReverseMap();
+        }
+    }
+}
