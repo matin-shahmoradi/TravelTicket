bfd73ef00fcc4edea81b3c865cbcfcf4b52fa37a
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-06-03 20:10:21 +0330

    refactor(Auth): Refactor user query services and interfaces
    Replaced UserQueryService with UserManagerQueryService and introduced IUserManagerQueryService interface. Updated user retrieval logic to use DTOs and added email uniqueness check. Removed old service and interface implementations.

---
+++ src/Services/Auth/AuthService/Interfaces/IUserManagerQueryService.cs
@@ -0,0 +1,12 @@
+﻿using AuthService.Model;
+using AuthService.Model.DTOs.UserDtos;
+
+namespace AuthService.Interfaces
+{
+    public interface IUserManagerQueryService
+    {
+        Task<IReadOnlyList<UserResponseDto>> GetUsersAsync(CancellationToken cancellationToken);
+        Task<ApplicationUser?> GetUserByEmailAsync(string email, CancellationToken cancellationToken);
+        Task<bool> IsEmailUniqueAsync(string email, CancellationToken cancellationToken);
+    }
+}
---
+++ src/Services/Auth/AuthService/Repositories/UserManagerQueryService.cs
@@ -0,0 +1,44 @@
+﻿using AuthService.Data;
+using AuthService.Interfaces;
+using AuthService.Model;
+using AuthService.Model.DTOs.UserDtos;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthService.Repositories
+{
+    public class UserManagerQueryService(
+        AuthDbContext authDbContext)
+        : IUserManagerQueryService
+    {
+        public async Task<IReadOnlyList<UserResponseDto>> GetUsersAsync(CancellationToken cancellationToken)
+        {
+            return await authDbContext
+                .Users
+                .AsNoTracking()
+                .Select(x => new UserResponseDto
+                {
+                    Id = x.Id,
+                    Email = x.Email!,
+                    UserName = x.UserName,
+                    PhoneNumber = x.PhoneNumber,
+                    FirstName = x.FirstName,
+                    LastName = x.LastName
+                }).ToListAsync(cancellationToken);
+        }
+
+        public async Task<ApplicationUser?> GetUserByEmailAsync(string email, CancellationToken cancellationToken)
+        {
+            return await authDbContext.Users
+                .Where(x => x.Email == email)
+                .FirstOrDefaultAsync(cancellationToken);
+        }
+
+        public async Task<bool> IsEmailUniqueAsync(string email, CancellationToken cancellationToken)
+        {
+            var normalizedEmail = email.ToUpperInvariant();
+            return !await authDbContext.Users
+                .AsNoTracking()
+                .AnyAsync(x => x.NormalizedEmail == normalizedEmail, cancellationToken);
+        }
+    }
+}
