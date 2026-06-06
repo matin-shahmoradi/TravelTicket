5eaed6d524481d9a06b54c5da28e25920d097d0c
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-06-06 16:13:02 +0330

    feat(Catalog): implemet CurrentUser for receiving current user information

---
+++ src/Services/Catalog/Catalog.API/Tickets/CurrentUser/CurrentUser.cs
@@ -0,0 +1,34 @@
+﻿using BuildingBlocks.Abstractions;
+using System.Security.Claims;
+
+namespace Catalog.API.Tickets.CurrentUser
+{
+    public class CurrentUser(IHttpContextAccessor httpContextAccessor)
+        : ICurrentUser
+    {
+        private ClaimsPrincipal? _principal =>
+            httpContextAccessor.HttpContext?.User;
+
+        public Guid? UserId
+        {
+            get
+            {
+                var value = _principal!.FindFirstValue(ClaimTypes.NameIdentifier);
+                return Guid.TryParse(value, out var userId) ? userId : null;
+            }
+        }
+
+        public string? UserName => _principal!.FindFirst(ClaimTypes.Name)!.Value;
+        public bool IsAuthenticated => _principal?.Identity?.IsAuthenticated ?? false;
+        public IReadOnlyCollection<string> Roles =>
+            _principal?
+                .FindAll(ClaimTypes.Role)
+                .Select(x => x.Value)
+                .ToArray() ?? [];
+
+        public bool IsInRole(string role)
+        {
+            return Roles.Contains(role, StringComparer.OrdinalIgnoreCase);
+        }
+    }
+}
