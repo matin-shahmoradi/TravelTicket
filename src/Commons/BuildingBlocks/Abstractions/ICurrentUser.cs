ad4f56ede14026cff7f4d6fa71d9e6eba56f6b03
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-06-06 14:32:34 +0330

    feat(BuildingBlock) : add ICurrentUser interface

---
+++ src/Commons/BuildingBlocks/Abstractions/ICurrentUser.cs
@@ -0,0 +1,15 @@
+﻿namespace BuildingBlocks.Abstractions
+{
+    public interface ICurrentUser
+    {
+        bool IsAuthenticated { get; }
+
+        Guid? UserId { get; }
+
+        string? UserName { get; }
+
+        IReadOnlyCollection<string> Roles { get; }
+
+        bool IsInRole(string role);
+    }
+}
