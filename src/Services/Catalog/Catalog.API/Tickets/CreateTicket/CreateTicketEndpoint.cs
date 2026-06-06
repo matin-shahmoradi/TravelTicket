3d412fce9a0e71a6e004c982190cb80f4526f12c
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-06-06 16:11:22 +0330

    feat(Catalog): secure create ticket endpoint and fix ToHttpResult method

--- src/Services/Catalog/Catalog.API/Tickets/CreateTicket/CreateTicketEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Tickets/CreateTicket/CreateTicketEndpoint.cs
@@ -1,5 +1,4 @@
-﻿using Catalog.API.CatalogExtensions;
-
+﻿using BuildingBlocks.Extensions;
 
 namespace Catalog.API.Tickets.CreateTicket
 {
@@ -7,7 +6,10 @@ namespace Catalog.API.Tickets.CreateTicket
     {
         public void AddRoutes(IEndpointRouteBuilder app)
         {
-            app.MapPost("/tickets" , async ([FromBody] TicketRequestDTO createRequest, ISender sender) =>
+            app.MapPost("/tickets", async (
+                ISender sender,
+                HttpContext context,
+                [FromBody] TicketRequestDTO createRequest) =>
             {
                 var requestResult = await sender.Send(new CreateTicketCommand(createRequest));
                 if (requestResult.IsSuccess)
@@ -15,12 +17,13 @@ namespace Catalog.API.Tickets.CreateTicket
                     return Results.Created($"/tickets/{requestResult.Value}", requestResult.Value);
                 }
 
-                return requestResult.ToHttpResult();
+                return requestResult.ToHttpResult(context);
             })
                 .WithName("CreateTicket")
                 .Produces(StatusCodes.Status201Created)
                 .ProducesProblem(StatusCodes.Status400BadRequest)
-                .WithSummary("this endpoint used for Create Ticket");
+                .WithSummary("this endpoint used for Create Ticket")
+                .RequireAuthorization("AdminOnly");
         }
     }
 }
