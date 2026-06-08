230d27a982582b0d2197bc77bcae6a3c7a106f05
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-06-08 20:14:04 +0330

    feat(BuildingBlock) : add shared serilog configuration for services

---
+++ src/Commons/BuildingBlocks/Extensions/SerilogExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace BuildingBlocks.Extensions
+{
+    public static class SerilogExtensions
+    {
+        public static IHostBuilder UseSharedSerilog(
+        this IHostBuilder hostBuilder,
+        IConfiguration configuration)
+        {
+            return hostBuilder.UseSerilog((context, services, loggerConfiguration) =>
+            {
+                loggerConfiguration
+                    .ReadFrom.Configuration(configuration)
+                    .ReadFrom.Services(services)
+                    .Enrich.FromLogContext();
+            });
+        }
+    }
+}
