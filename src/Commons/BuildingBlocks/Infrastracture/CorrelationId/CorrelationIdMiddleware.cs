2adb8dab8c2763c82ba749bdd06969477843bb82
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-06-08 19:49:22 +0330

    feat(BuildingBlock) : implement correlationId middleware for track requests
    - implement CorrleationIdMiddleware to recieve correlation id from the header

---
+++ src/Commons/BuildingBlocks/Infrastracture/CorrelationId/CorrelationIdMiddleware.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Serilog.Context;
+
+namespace BuildingBlocks.Infrastracture.CorrelationId
+{
+    public sealed class CorrelationIdMiddleware(RequestDelegate next)
+    {
+        public async Task Invoke(HttpContext context)
+        {
+            var correlationId = context.Request.Headers[CorrelationConstants.HeaderName].FirstOrDefault();
+
+            if (string.IsNullOrWhiteSpace(correlationId))
+                correlationId = Guid.NewGuid().ToString();
+
+            context.Items[CorrelationConstants.HeaderName] = correlationId;
+
+            context.Response.OnStarting(() =>
+            {
+                context.Response.Headers[CorrelationConstants.HeaderName] = correlationId;
+                return Task.CompletedTask;
+            });
+
+            using (LogContext.PushProperty(CorrelationConstants.LogPropertyName, correlationId))
+            {
+                await next(context);
+            }
+        }
+    }
+}
