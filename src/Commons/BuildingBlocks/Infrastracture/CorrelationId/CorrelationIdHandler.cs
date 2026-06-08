31539da15f50e2b3abc4861e110b626eaf0907b5
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-06-08 19:53:35 +0330

    feat(BuildingBlock): implement CorrelationIdHandler to send current correlation id to next request header

---
+++ src/Commons/BuildingBlocks/Infrastracture/CorrelationId/CorrelationIdHandler.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace BuildingBlocks.Infrastracture.CorrelationId
+{
+    public sealed class CorrelationIdHandler(IHttpContextAccessor httpContextAccessor) : DelegatingHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var context = httpContextAccessor.HttpContext;
+
+            if (context != null &&
+                context.Items.TryGetValue(CorrelationConstants.HeaderName, out var correlationId))
+            {
+                request.Headers.TryAddWithoutValidation(
+                    CorrelationConstants.HeaderName,
+                    correlationId!.ToString());
+            }
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
