06f1985b9a9e599d53fa2251f7bedecdbc5bf349
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-06-08 19:51:37 +0330

    feat(BuildingBlock): add X-Correlation-Id and CorrleationId Constants

---
+++ src/Commons/BuildingBlocks/Infrastracture/CorrelationId/CorrelationConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace BuildingBlocks.Infrastracture.CorrelationId
+{
+    public static class CorrelationConstants
+    {
+        public const string HeaderName = "X-Correlation-ID";
+        public const string LogPropertyName = "CorrelationId";
+    }
+}
