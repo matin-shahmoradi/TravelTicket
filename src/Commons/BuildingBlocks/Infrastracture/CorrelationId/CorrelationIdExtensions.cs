3d74036472846127997767ad29d776bcf33a6b77
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-06-08 20:10:34 +0330

    feat(BuildingBlock) : add service collection class for correlationId implementasions

---
+++ src/Commons/BuildingBlocks/Infrastracture/CorrelationId/CorrelationIdExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BuildingBlocks.Infrastracture.CorrelationId
+{
+    public static class CorrelationIdExtensions
+    {
+        public static IServiceCollection AddCorrelationId(this IServiceCollection services)
+        {
+            services.AddScoped<CorrelationIdHandler>();
+            return services;
+        }
+        public static IApplicationBuilder UseCorrelationId(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<CorrelationIdMiddleware>();
+            return app;
+        }
+
+    }
+}
