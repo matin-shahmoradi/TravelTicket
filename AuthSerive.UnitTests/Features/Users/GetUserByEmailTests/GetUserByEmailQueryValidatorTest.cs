d7950330773e37c508ea7847931ceaf121e8e81d
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-06-11 14:48:52 +0330

    feat(test) : add unit tests for GetUserByEmailQueryValidator
    Introduce GetUserByEmailQueryValidatorTest to verify email validation logic, including tests for both valid and invalid email scenarios.

---
+++ AuthSerive.UnitTests/Features/Users/GetUserByEmailTests/GetUserByEmailQueryValidatorTest.cs
@@ -0,0 +1,41 @@
+﻿using AuthService.Auth.GetUsersByEmail;
+using FluentAssertions;
+
+namespace AuthSerive.UnitTests.Features.Users.GetUserByEmailTests
+{
+    public class GetUserByEmailQueryValidatorTest
+    {
+        private readonly GetUserByEmailQueryValidator _validator = new();
+        [Fact]
+        public void Validate_ShouldPass_WhenEmailIsValid()
+        {
+            // Arrange
+            var query = new GetUserByEmailQuery("example@gmail.com");
+
+            // Act
+            var result = _validator.Validate(query);
+
+            // Assert
+            result.IsValid.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData("invalid-email")]
+        [InlineData("user@")]
+        [InlineData("@invalidEmail")]
+        public void Validate_ShouldFail_WhenEmailIsInvalid(string email)
+        {
+            // Arrange 
+            var query = new GetUserByEmailQuery(email);
+
+            // Act
+            var result = _validator.Validate(query);
+
+            // Assert
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().NotBeEmpty();
+        }
+    }
+}
