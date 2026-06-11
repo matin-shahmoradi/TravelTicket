8583857b5f513dccea9e1d3b337a024421638dd0
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-06-11 14:37:34 +0330

    feat(auth) : add FluentEmailOptions class for email configuration
    Introduced the AuthService.OptionProperties namespace and added the FluentEmailOptions class with properties for SenderEmail, Sender, Host, and Port to support email configuration.

---
+++ src/Services/Auth/AuthService/Options/FluentEmailOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace AuthService.OptionProperties
+{
+    public class FluentEmailOptions
+    {
+        public string SenderEmail { get; set; } = default!;
+        public string Sender { get; set; } = default!;
+        public string Host { get; set; } = default!;
+        public string Port { get; set; } = default!;
+    }
+}
