297b82f082dcbcd384ff3d74cffb0519f7e8ae85
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-06-11 14:30:34 +0330

    feat(auth): add FluentEmailSender for registration email delivery
    Implemented IFluentEmailSender interface and its FluentEmailSender class to handle sending registration emails with email confirmation links using FluentEmail. Integrated UserManager and configuration options for token generation and URL construction.

---
+++ src/Services/Auth/AuthService/Interfaces/IFluentEmailSender.cs
@@ -0,0 +1,10 @@
+﻿using AuthService.Model;
+using FluentEmail.Core.Models;
+
+namespace AuthService.Interfaces
+{
+    public interface IFluentEmailSender
+    {
+        Task<SendResponse> SendEmailRegisteration(ApplicationUser user, CancellationToken cancellationToken);
+    }
+}
---
+++ src/Services/Auth/AuthService/Repositories/FluentEmailSender.cs
@@ -0,0 +1,44 @@
+﻿using AuthService.Interfaces;
+using AuthService.Model;
+using AuthService.Options;
+using FluentEmail.Core;
+using FluentEmail.Core.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Options;
+using System.Text;
+
+namespace AuthService.Repositories
+{
+    public class FluentEmailSender : IFluentEmailSender
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IFluentEmail _fluentEmail;
+        private readonly EmailConfirmationUrlOptions _emailConfirmationUrlOptions;
+        public FluentEmailSender(
+            UserManager<ApplicationUser> userManager,
+            IFluentEmail fluentEmail,
+            IOptions<EmailConfirmationUrlOptions> emailConfirmationUrlOptions)
+        {
+            _userManager = userManager;
+            _fluentEmail = fluentEmail;
+            _emailConfirmationUrlOptions = emailConfirmationUrlOptions.Value;
+        }
+        public async Task<SendResponse> SendEmailRegisteration(ApplicationUser user, CancellationToken cancellationToken)
+        {
+            string emailVerificationToken = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+
+            string encodedToken = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(emailVerificationToken));
+
+            string baseUrl = _emailConfirmationUrlOptions.httpUrl;
+            string callbackUrl = $"{baseUrl}?userId={user.Id}&token={encodedToken}";
+
+            return await _fluentEmail
+                    .To(user.Email)
+                    .Subject("Email verification for TravelTicket")
+                    .Body($@"Hello {user.FirstName} 
+                            to verify your email address click here <a href='{callbackUrl}'>Verify Email</a>", isHtml: true)
+                    .SendAsync(cancellationToken);
+        }
+    }
+}
