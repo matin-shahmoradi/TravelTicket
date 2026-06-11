23ae84ca3ea396a9ebcb55cd70e73afa59183ca7
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-06-11 14:37:14 +0330

    feat(auth) : add EmailConfirmationUrlOptions class for URL settings
    Introduced EmailConfirmationUrlOptions in AuthService.Options namespace with httpUrl and httpsUrl properties to manage email confirmation URLs.

---
+++ src/Services/Auth/AuthService/Options/EmailConfirmationUrlOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace AuthService.Options
+{
+    public class EmailConfirmationUrlOptions
+    {
+        public string httpUrl { get; set; } = default!;
+        public string httpsUrl { get; set; } = default!;
+    }
+}
