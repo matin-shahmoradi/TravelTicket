aeb7084cae9db1e1a01036eedaf5fc02c55cae9f
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-06-11 14:41:20 +0330

    refactor(auth) : refactor ConfirmEmail handler to use service interfaces
    Refactored ConfirmEmailCommandHandler to use IUserManagerCommandService and IUserManagerQueryService for user operations, improving separation of concerns and testability. Updated error handling and adjusted using directives for new dependencies.

--- src/Services/Auth/AuthService/Auth/ConfirmEmail/ConfirmEmailCommandHandler.cs
+++ src/Services/Auth/AuthService/Auth/ConfirmEmail/ConfirmEmailCommandHandler.cs
@@ -1,26 +1,28 @@
-﻿using AuthService.Model;
+﻿using AuthService.Interfaces;
 using BuildingBlocks;
 using BuildingBlocks.CQRS;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.WebUtilities;
 using System.Text;
 
 namespace AuthService.Auth.ConfirmEmail
 {
-    internal sealed class ConfirmEmailCommandHandler(UserManager<ApplicationUser> userManager) : ICommandHandler<ConfirmEmailCommand, Result<string>>
+    internal sealed class ConfirmEmailCommandHandler(
+        IUserManagerCommandService userManagerCommandService,
+        IUserManagerQueryService userManagerQueryService)
+        : ICommandHandler<ConfirmEmailCommand, Result<string>>
     {
         public async Task<Result<string>> Handle(ConfirmEmailCommand command, CancellationToken cancellationToken)
         {
-            var user = await userManager.FindByIdAsync(command.UserId);
+            var user = await userManagerQueryService.GetUserByIdAsync(command.UserId, cancellationToken);
             if (user is null)
                 return Result<string>.Failure(Error.NotFoundError(message: "User not found!"));
 
             var decodedToken = Encoding.UTF8.GetString(WebEncoders.Base64UrlDecode(command.EmailConfirmationToken));
 
-            var result = await userManager.ConfirmEmailAsync(user, decodedToken);
+            var result = await userManagerCommandService.ConfirmUserEmailAsync(user, decodedToken);
 
             if (!result.Succeeded)
-                return Result<string>.Failure(Error.CustomError(mesaage: $"{result.Errors}"));
+                return Result<string>.Failure(Error.Internal_Server(message: $"{result.Errors}"));
 
             return Result<string>.Success($"Your email confirmed successfully");
         }
