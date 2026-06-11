6926daa44d651c243ccaa773c1b743a35a8bcd41
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-06-11 14:52:35 +0330

    feat(test) :  add unit tests for ConfirmEmailCommandValidator
    Introduced ConfirmEmailCommandValidatorTest to cover validation scenarios for user ID and email confirmation token. Tests include valid and invalid inputs using xUnit and FluentAssertions.

---
+++ AuthSerive.UnitTests/Features/ConfirmEmail/ConfirmEmailCommandValidatorTest.cs
@@ -0,0 +1,59 @@
+﻿using AuthService.Auth.ConfirmEmail;
+using FluentAssertions;
+
+namespace AuthService.UnitTests.Features.ConfirmEmail
+{
+    public class ConfirmEmailCommandValidatorTest
+    {
+        private readonly ConfirmEmailCommandValidator _validator = new();
+
+        [Fact]
+        public void Validate_ShouldPass_WhenUserIdAndTokenAreValid()
+        {
+            var command = new ConfirmEmailCommand
+                (
+                    UserId: Guid.NewGuid().ToString(),
+                    EmailConfirmationToken: Guid.NewGuid().ToString()
+                );
+
+            var result = _validator.Validate(command);
+
+            result.IsValid.Should().BeTrue();
+
+            result.Errors.Count.Should().Be(0);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void Validate_ShouldFail_WhenUserIdIsEmpty(string userId)
+        {
+            var command = new ConfirmEmailCommand(
+                UserId: userId,
+                EmailConfirmationToken: Guid.NewGuid().ToString()
+                );
+
+            var result = _validator.Validate(command);
+
+            result.IsValid.Should().BeFalse();
+            result.Errors.Count.Should().NotBe(0);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void Validate_ShouldFail_WhenTokenIsEmpty(string emailConfirmationToken)
+        {
+            var command = new ConfirmEmailCommand(
+                UserId: Guid.NewGuid().ToString(),
+                EmailConfirmationToken: emailConfirmationToken
+                );
+
+            var result = _validator.Validate(command);
+
+            result.IsValid.Should().BeFalse();
+            result.Errors.Count.Should().NotBe(0);
+        }
+    }
+
+}
