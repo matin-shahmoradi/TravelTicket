103f32931b5fa49a8128b06fcbb8950d0c6a11c1
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-06-11 14:35:54 +0330

    refactor(auth) : refactor registration handler to use service abstractions
    Replaced direct UserManager and FluentEmail usage with new abstraction interfaces for user management and email sending. Improved error messages, streamlined email verification logic, enhanced error handling, and removed obsolete dependencies.

--- src/Services/Auth/AuthService/Auth/Register/UserRegisterCommandHandler.cs
+++ src/Services/Auth/AuthService/Auth/Register/UserRegisterCommandHandler.cs
@@ -1,29 +1,25 @@
-﻿using AuthService.Model;
+﻿using AuthService.Interfaces;
+using AuthService.Model;
 using AuthService.Model.DTOs.RegisterDtos;
 using BuildingBlocks;
 using BuildingBlocks.CQRS;
-using FluentEmail.Core;
-using FluentEmail.Core.Models;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.WebUtilities;
-using System.Text;
 
 namespace AuthService.Auth.Register
 {
     public class UserRegisterCommandHandler(
-        UserManager<ApplicationUser> userManager,
-        IFluentEmail fluentEmail,
-        IConfiguration configuration,
+        IUserManagerQueryService userManagerQueryService,
+        IUserManagerCommandService userManagerCommandService,
+        IFluentEmailSender emailSender,
         ILogger<UserRegisterCommandHandler> logger)
         : ICommandHandler<UserRegisterCommand, Result<RegisterResponseDto>>
     {
         public async Task<Result<RegisterResponseDto>> Handle(UserRegisterCommand command, CancellationToken cancellationToken)
         {
-            var userExistCheck = await userManager.FindByEmailAsync(command.Request.Email);
+            var userExistCheck = await userManagerQueryService.GetUserByEmailAsync(command.Request.Email, cancellationToken);
 
             if (userExistCheck is not null)
                 return Result<RegisterResponseDto>
-                    .Failure(Error.UnAuthorized(message: "user is already exist."));
+                    .Failure(Error.Conflict(message: "Email is already exist."));
 
             var newUser = ApplicationUser.CreateUser(
                 email: command.Request.Email,
@@ -31,18 +27,18 @@ namespace AuthService.Auth.Register
                 firstname: command.Request.Firstname,
                 lastname: command.Request.Lastname);
 
-            var createUser = await userManager.CreateAsync(newUser, command.Request.Password);
+            var createUser = await userManagerCommandService.CreateUserAsync(newUser, command.Request.Password);
 
             if (!createUser.Succeeded)
                 return Result<RegisterResponseDto>.Failure(Error.CustomError());
 
 
-            var addUserToRole = await userManager.AddToRoleAsync(newUser, Roles.User);
+            var addUserToRole = await userManagerCommandService.AddUserToRoleAsync(newUser, Roles.User);
             if (!addUserToRole.Succeeded)
                 return Result<RegisterResponseDto>
-                    .Failure(Error.Internal_Server(mesaage: $" Cant assign user with id {newUser.Id} to {Roles.User} role"));
+                    .Failure(Error.Internal_Server(message: $" Cant assign user with id {newUser.Id} to {Roles.User} role"));
 
-            var emailValidationResponse = await SendValidatationEmail(newUser, cancellationToken);
+            var emailValidationResponse = await emailSender.SendEmailRegisteration(newUser, cancellationToken);
 
             if (!emailValidationResponse.Successful)
             {
@@ -52,27 +48,12 @@ namespace AuthService.Auth.Register
                     newUser.Email,
                     emailValidationResponse.ErrorMessages
                     );
+                return Result<RegisterResponseDto>.Failure(
+                    Error.Internal_Server(message: $"failed to send verification email : {emailValidationResponse.ErrorMessages}"));
             }
             var userRegisterResult = new RegisterResponseDto(command.Request.Email, $"{command.Request.Firstname} {command.Request.Lastname}");
 
             return Result<RegisterResponseDto>.Success(userRegisterResult);
-        }
-
-        private async Task<SendResponse> SendValidatationEmail(ApplicationUser user, CancellationToken cancellationToken)
-        {
-            string emailVerificationToken = await userManager.GenerateEmailConfirmationTokenAsync(user);
-
-            string encodedToken = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(emailVerificationToken));
-
-            string baseUrl = configuration["EmailConfirmationUrl:httpUrl"]!;
-            string callbackUrl = $"{baseUrl}?userId={user.Id}&token={encodedToken}";
-
-            return await fluentEmail
-                    .To(user.Email)
-                    .Subject("Email verification for TravelTicket")
-                    .Body($@"Hello {user.FirstName} 
-                            to verify your email address click here <a href='{callbackUrl}'>Verify Email</a>", isHtml: true)
-                    .SendAsync(cancellationToken);
         }
     }
 }
