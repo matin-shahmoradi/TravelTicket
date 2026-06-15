3ce5fc58391375660813a57478017b755d5a02b5
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-06-15 18:50:03 +0330

    feat(basket) : update AddItemToBasket endpoint route and metadata
    Changed the route to remove the username parameter, now using "baskets/items". Added endpoint metadata for better documentation.

--- src/Services/Basket/Basket.API/Basket/AddItemToBasket/AddItemToBasketEndpoint.cs
+++ src/Services/Basket/Basket.API/Basket/AddItemToBasket/AddItemToBasketEndpoint.cs
@@ -6,9 +6,9 @@ namespace Basket.API.Basket.AddItemToBasket
     {
         public void AddRoutes(IEndpointRouteBuilder app)
         {
-            app.MapPost("baskets/{username}/items", async (
+            app.MapPost("baskets/items", async (
                 HttpContext context,
-                ISender sender, 
+                ISender sender,
                 [FromBody] AddItemToBasketDto dto) =>
             {
                 var command = new AddItemToBasketCommand(dto);
@@ -21,7 +21,13 @@ namespace Basket.API.Basket.AddItemToBasket
                 }
                 // TODO : Redirect user to GetBasket Endpoint.
                 return Results.Ok(addItemToBasket);
-            });
+            })
+                .WithName("AddItemToBasket")
+                .WithSummary("used for add item to user basket")
+                .Produces(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status400BadRequest)
+                .ProducesProblem(StatusCodes.Status401Unauthorized)
+                .ProducesProblem(StatusCodes.Status500InternalServerError);
         }
     }
 }
