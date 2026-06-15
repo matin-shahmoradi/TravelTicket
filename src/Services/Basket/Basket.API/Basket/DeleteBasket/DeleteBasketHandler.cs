b0c7f5baffc79a8e03da3e3727dee17103ddd814
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-06-15 23:50:20 +0330

    refactor(basket) : refactor DeleteBasket to use current user context
    - remove Username from DeleteBasketCommand
    - update handler to use ICurrentUser for basket operations.
    - replace all username references with currentUser.UserName.

--- src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
@@ -1,20 +1,23 @@
-﻿namespace Basket.API.Basket.DeleteBasket
+﻿using BuildingBlocks.Abstractions;
+
+namespace Basket.API.Basket.DeleteBasket
 {
-    public record DeleteBasketCommand(string Username) : ICommand<Result<string>>;
-    internal sealed class DeleteBasketCommandHandler(IBasketRepository basketRepository)
+    public record DeleteBasketCommand : ICommand<Result<string>>;
+    internal sealed class DeleteBasketCommandHandler(
+        IBasketRepository basketRepository,
+        ICurrentUser currentUser)
         : ICommandHandler<DeleteBasketCommand, Result<string>>
     {
         public async Task<Result<string>> Handle(DeleteBasketCommand command, CancellationToken cancellationToken)
         {
-            var existedBasket = await basketRepository.GetBasket(command.Username);
+            var existedBasket = await basketRepository.GetBasket(currentUser.UserEmail!);
             if (existedBasket is not null)
             {
-                await basketRepository.DeleteBasket(command.Username);
+                await basketRepository.DeleteBasket(currentUser.UserEmail!);
                 return Result<string>.Success($"Basket with Id: {existedBasket.Id} deleted successfully.");
             }
 
             return Result<string>.Failure(Error.NotFoundError(message: "Basket Not Found!"));
-
         }
     }
 }
