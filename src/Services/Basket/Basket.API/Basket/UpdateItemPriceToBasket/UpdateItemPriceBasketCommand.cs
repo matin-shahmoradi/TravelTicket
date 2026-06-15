983756ef5950449d02be0077d75d192956a4d928
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-06-15 23:57:52 +0330

    feat(basket) : add validator for UpdateItemPriceBasketCommand
    - add validator for TicketId and Price

--- src/Services/Basket/Basket.API/Basket/UpdateItemPriceToBasket/UpdateItemPriceBasketCommand.cs
+++ src/Services/Basket/Basket.API/Basket/UpdateItemPriceToBasket/UpdateItemPriceBasketCommand.cs
@@ -1,4 +1,16 @@
 ﻿namespace Basket.API.Basket.UpdateItemPriceInBasket
 {
     public record UpdateItemPriceBasketCommand(Guid TicketId, decimal Price) : ICommand<Result<bool>>;
+
+    public class UpdateItemPriceBasketCommandValidator : AbstractValidator<UpdateItemPriceBasketCommand>
+    {
+        public UpdateItemPriceBasketCommandValidator()
+        {
+            RuleFor(x => x.TicketId)
+                .NotEmpty().WithMessage("ticket id cant be empty.");
+
+            RuleFor(x => x.Price)
+                .GreaterThanOrEqualTo(0).WithMessage("ticket price cant be less than zero");
+        }
+    }
 }
