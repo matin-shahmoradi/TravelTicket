1139628d8a78edc779cc8955aed84bd6bedb6e80
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-06-16 00:02:35 +0330

    feat(catalog) : add CatalogRpcService with GetTicketById gRPC method
    - Implemented CatalogRpcService in the Catalog.API.Grpc namespace.
    - The service uses ICatalogDbContext to fetch tickets by ID, returning ticket details or a gRPC NotFound error if the ticket does not exist.

---
+++ src/Services/Catalog/Catalog.API/Grpc/CatalogRpcService.cs
@@ -0,0 +1,26 @@
+﻿using Catalog.API.Domain.ValueObjects;
+using Catalog.Grpc;
+using Grpc.Core;
+using Microsoft.EntityFrameworkCore;
+namespace Catalog.API.Grpc
+{
+    public class CatalogRpcService(ICatalogDbContext catalogDbContext) : CatalogGrpcService.CatalogGrpcServiceBase
+    {
+        public override async Task<GetTicketByIdResponse> GetTicketById(GetTicketByIdRequest request, ServerCallContext context)
+        {
+            var id = TicketId.New(Guid.Parse(request.TicketId));
+            var ticket = await catalogDbContext.Tickets.FirstOrDefaultAsync(x => x.Id == id);
+            if (ticket is null)
+                throw new RpcException(new Status(StatusCode.NotFound, $"Ticket with id : {request.TicketId} Not Found!"));
+
+            return new GetTicketByIdResponse
+            {
+                TicketId = ticket.Id.Value.ToString(),
+                Origin = ticket.Origin,
+                Destination = ticket.Destination,
+                TravelDate = ticket.TravelDate.ToString(),
+                Price = (double)ticket.Price,
+            };
+        }
+    }
+}
