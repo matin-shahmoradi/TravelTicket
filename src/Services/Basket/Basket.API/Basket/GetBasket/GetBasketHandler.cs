f574f1a7ed61242c9f34d9ee9f9c04397b2e9dd5
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-06-16 00:08:05 +0330

    refactor(basket) : refactor GetBasketQuery to use current user context
     - removed username from GetBasketQuery and injected ICurrentUser into the handler.

--- src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -1,14 +1,17 @@
 ﻿using Basket.API.Common.Dtos.MapExtensions;
+using BuildingBlocks.Abstractions;
 
 namespace Basket.API.Basket.GetBasket
 {
-    public record GetBasketQuery(string username) : IQuery<Result<ShoppingCartDto>>;
-    internal class GetBasketQueryHandler(IBasketRepository basketRepository)
+    public record GetBasketQuery : IQuery<Result<ShoppingCartDto>>;
+    internal class GetBasketQueryHandler(
+        IBasketRepository basketRepository,
+        ICurrentUser currentUser)
         : IQueryHandler<GetBasketQuery, Result<ShoppingCartDto>>
     {
         public async Task<Result<ShoppingCartDto>> Handle(GetBasketQuery request, CancellationToken cancellationToken)
         {
-            var basket = await basketRepository.GetBasket(request.username);
+            var basket = await basketRepository.GetBasket(currentUser.UserName!);
             if (basket is null)
             {
                 return Result<ShoppingCartDto>.Failure
