80333be564123dabc32c91f5d8d8f1b79749396b
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-06-15 18:47:54 +0330

    feat(basket) : add CurrentUser service for accessing user claims
    Introduced CurrentUser class in Basket.API.Services implementing ICurrentUser. This service uses IHttpContextAccessor to provide access to user claims such as UserId, UserName, UserEmail, authentication status, and roles, with exception handling for missing claims.

---
+++ src/Services/Basket/Basket.API/Services/CurrentUser.cs
@@ -0,0 +1,35 @@
+﻿using BuildingBlocks.Abstractions;
+using System.Security.Claims;
+
+namespace Basket.API.Services
+{
+    public class CurrentUser(IHttpContextAccessor httpContextAccessor)
+        : ICurrentUser
+    {
+        private ClaimsPrincipal? _principal =>
+            httpContextAccessor.HttpContext?.User;
+
+        public Guid? UserId
+        {
+            get
+            {
+                var value = _principal?.FindFirstValue(ClaimTypes.NameIdentifier);
+                return Guid.TryParse(value, out var userId) ? userId : throw new Exception("Null User id");
+            }
+        }
+
+        public string? UserName => _principal?.FindFirst(ClaimTypes.Name)?.Value ?? throw new Exception("Null Username");
+        public string? UserEmail => _principal?.FindFirst(ClaimTypes.Email)?.Value ?? throw new Exception("Null User email ");
+        public bool IsAuthenticated => _principal?.Identity?.IsAuthenticated ?? false;
+        public IReadOnlyCollection<string> Roles =>
+            _principal?
+                .FindAll(ClaimTypes.Role)
+                .Select(x => x.Value)
+                .ToArray() ?? [];
+
+        public bool IsInRole(string role)
+        {
+            return Roles.Contains(role, StringComparer.OrdinalIgnoreCase);
+        }
+    }
+}
