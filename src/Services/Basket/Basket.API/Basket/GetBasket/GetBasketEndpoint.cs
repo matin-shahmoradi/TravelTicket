8c7557a50bf658d71de036018b94756c3929f7cb
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-06-15 23:23:08 +0330

    feat(basket) : update basket endpoint to remove username parameter
    - changed the basket retrieval route from "baskets/{username}" to "/baskets"
    - removing the need for a username parameter.
    - updated the handler to call GetBasketQuery without a username.
    - added endpoint metadata including name, summary, response codes, and authorization requirement.

--- src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoint.cs
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoint.cs
@@ -4,14 +4,21 @@
     {
         public void AddRoutes(IEndpointRouteBuilder app)
         {
-            app.MapGet("baskets/{username}",async (HttpContext context,ISender sender ,string username) =>
+            app.MapGet("/baskets", async (HttpContext context, ISender sender) =>
             {
-                var getBasket = await sender.Send(new GetBasketQuery(username));
-                if(!getBasket.IsSuccess)
+                var getBasket = await sender.Send(new GetBasketQuery());
+                if (!getBasket.IsSuccess)
                     return getBasket.ToHttpResult(context);
 
                 return Results.Ok(getBasket);
-            });
+            })
+                .WithName("GetBasket")
+                .WithSummary("Get user basket")
+                .Produces(StatusCodes.Status200OK)
+                .ProducesProblem(StatusCodes.Status401Unauthorized)
+                .ProducesProblem(StatusCodes.Status404NotFound)
+                .ProducesProblem(StatusCodes.Status500InternalServerError)
+                .RequireAuthorization("User");
         }
     }
 }
