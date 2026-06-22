4f8a618d458730166faa0458722c452610411fa9
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-06-23 00:41:37 +0330

    feat(buildingBlock) : add BasketCheckOutIntegrationEvent and related item record

---
+++ src/Commons/BuildingBlocks/Messaging/Events/BasketEvents/BasketCheckOutIntegrationEvent.cs
@@ -0,0 +1,12 @@
+﻿namespace BuildingBlocks.Messaging.Events.BasketEvents
+{
+    public class BasketCheckOutIntegrationEvent : IntegrationEvent
+    {
+        public Guid CustomerId { get; set; }
+        public string Email { get; set; } = default!;
+        public string PhoneNumber { get; set; } = default!;
+        public List<BasketCheckOutIntegrationEventItem> Items = new();
+    }
+
+    public record BasketCheckOutIntegrationEventItem(Guid TicketId, int Quantity, decimal Price);
+}
