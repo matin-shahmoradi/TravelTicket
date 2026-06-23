526451188f00220f5eef0441da26e5475b378ecf
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-06-23 23:52:22 +0330

    feat(basket) : add BasketCheckOutEventHandler
    - added BasketCheckOutEventHandler  to handle BasketCheckOutEvent domain event

---
+++ src/Services/Basket/Basket.API/EventHandlers/BasketEvents/BasketCheckOutEventHandler.cs
@@ -0,0 +1,34 @@
+﻿using Basket.API.Events;
+using BuildingBlocks.Abstractions;
+using BuildingBlocks.Messaging.Events.BasketEvents;
+using MassTransit;
+
+namespace Basket.API.EventHandlers.BasketEvents
+{
+    public class BasketCheckOutEventHandler(
+        IBus bus,
+        ICurrentUser currentUser,
+        ILogger<BasketCheckOutEventHandler> logger)
+        : INotificationHandler<BasketCheckOutEvent>
+    {
+        public Task Handle(BasketCheckOutEvent notification, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("[EVENT] Event handled : {Event}", notification.GetType().Name);
+            //TODO : add customer phone number in auth service
+            var BasketCheckoutedIntegrationEvent = new BasketCheckOutIntegrationEvent
+            {
+                CustomerId = currentUser.UserId!,
+                Email = currentUser.UserEmail!,
+                PhoneNumber = null!,
+                Items = notification.Items.Select(x => new BasketCheckOutIntegrationEventItem
+                (
+                    TicketId: x.TicketId,
+                    Price: x.Price,
+                    Quantity: x.Quantity
+                )).ToList()
+            };
+
+            return bus.Publish(BasketCheckoutedIntegrationEvent);
+        }
+    }
+}
