bde422ab349e0cc896e4951ca5e36cb47f65374a
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-06-24 00:08:45 +0330

    refactor(basket) : update IBasketRepository to use Guid customerId
    Replaced string username with Guid customerId in basket methods. Added QueryTrackingBehavior parameter to GetBasket and included necessary using directive for Entity Framework Core.

--- src/Services/Basket/Basket.API/Data/Repositories/IBasketRepository.cs
+++ src/Services/Basket/Basket.API/Data/Repositories/IBasketRepository.cs
@@ -1,9 +1,11 @@
-﻿namespace Basket.API.Data.Repository
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Basket.API.Data.Repository
 {
     public interface IBasketRepository
     {
-        Task<ShoppingCart> GetBasket(string username , CancellationToken cancellation = default);
+        Task<ShoppingCart?> GetBasket(Guid customerId, QueryTrackingBehavior tracking = QueryTrackingBehavior.NoTracking, CancellationToken cancellation = default);
         Task<ShoppingCart> StoreBasket(ShoppingCart basket, CancellationToken cancellation = default);
-        Task<bool> DeleteBasket(string username, CancellationToken cancellation = default);
+        Task<bool> DeleteBasket(Guid customerId, CancellationToken cancellation = default);
     }
 }
