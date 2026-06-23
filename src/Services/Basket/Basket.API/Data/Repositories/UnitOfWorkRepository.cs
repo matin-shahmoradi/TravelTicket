9fa5d1cba388f82c32162cdeda93d0bb839a8c1a
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-06-23 23:59:14 +0330

    feat(basket) : add UnitOfWorkRepository implementation for Basket API

---
+++ src/Services/Basket/Basket.API/Data/Repositories/UnitOfWorkRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Basket.API.Data.Repositories
+{
+    public sealed class UnitOfWorkRepository(BacketDbContext basketDbContext) : IUnitOfWork
+    {
+        public async Task SaveChangesAsync(CancellationToken cancellation) =>
+            await basketDbContext.SaveChangesAsync(cancellation);
+    }
+}
