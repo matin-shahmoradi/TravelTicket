d1c1a0ff288c2d6c4766aadced6f8d1fb367cbe8
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-06-25 18:55:05 +0330

    build(order) : add national code index for customer table

---
+++ src/Services/Ordering/Ordering.Infrastructure/Migrations/20260625152342_add_nationalCode_Index.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ordering.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_nationalCode_Index : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_NationalCode",
+                table: "Customers",
+                column: "NationalCode");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Customers_NationalCode",
+                table: "Customers");
+        }
+    }
+}
