45d4f1eab945e05573f40848f841965f6c3a7593
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-06-25 19:35:34 +0330

    refactor(order) : refactor order creation and validation logic
    - Removed OrderName and CustomerId validation from CreateOrderValidator.
    - Refactored CreateOrderHandler to add orders and customers asynchronously with error handling.
    - Updated logging and changed CreateNewOrder to return both Order and Customer entities.

--- src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderCommand.cs
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderCommand.cs
@@ -6,8 +6,6 @@
     {
         public CreateOrderValidator()
         {
-            RuleFor(x => x.OrderDto.OrderName).NotEmpty().WithMessage("Name is required.");
-            RuleFor(x => x.OrderDto.CustomerId).NotNull().WithMessage("CustomerId is required.");
             RuleFor(x => x.OrderDto.OrderItems).NotEmpty().WithMessage("Order Items should not be empty.");
         }
     }
--- src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderHandler.cs
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderHandler.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using Ordering.Domain.ValueObjects.IdValueObjects;
 
 namespace Ordering.Application.Orders.Commands.CreateOrder
 {
-    internal sealed class CreateOrderHandler(
+    public sealed class CreateOrderHandler(
         IOrderDbContext orderContext,
         ILogger<CreateOrderHandler> logger)
         : ICommandHandler<CreateOrderCommand, Result<Guid>>
@@ -11,38 +12,47 @@ namespace Ordering.Application.Orders.Commands.CreateOrder
         public async Task<Result<Guid>> Handle(CreateOrderCommand command, CancellationToken cancellationToken)
         {
             var order = CreateNewOrder(command.OrderDto);
-
-            orderContext.Orders.Add(order);
+            var customer = await orderContext.Customers
+                .Where(x => x.Id == order.Item2.Id)
+                .FirstOrDefaultAsync(cancellationToken);
+            try
+            {
+                await orderContext.Orders.AddAsync(order.Item1);
+                await orderContext.Customers.AddAsync(order.Item2);
+            }
+            catch (Exception e)
+            {
+                logger.LogError("Failed to create new order. error : {error} ", e.Message);
+                return Result<Guid>.Failure(Error.Internal_Server(message: e.Message));
+            }
             await orderContext.SaveChangesAsync(cancellationToken);
 
-            logger.LogInformation(
-                "Order : {orderId} Created for Customer : {customerId}",
-                order.Id, order.CustomerId);
-
+            logger.LogInformation("Order created successfully : {order}", order);
             return Result<Guid>.Success(command.OrderDto.Id);
         }
 
-        private Order CreateNewOrder(OrderDto orderDto)
+        private (Order, Customer) CreateNewOrder(OrderDto orderDto)
         {
             var newOrder = Order.Create(
                 id: OrderId.New(),
-                customerId: CustomerId.New(orderDto.Id),
-                orderName: OrderName.New(orderDto.OrderName),
-                payment: Payment.New(
-                    orderDto.Payment.CardName,
-                    orderDto.Payment.CardNumber,
-                    orderDto.Payment.Expiration,
-                    orderDto.Payment.CVV,
-                    orderDto.Payment.PaymentMethod));
+                customerId: CustomerId.New(orderDto.Customer.CustomerId));
 
-            foreach(var orderItemDto in orderDto.OrderItems)
+            foreach (var orderItemDto in orderDto.OrderItems)
             {
                 newOrder.Add(
                     ticketId: TicketId.New(orderItemDto.TicketId),
                     quantity: orderItemDto.Quantity,
                     price: orderItemDto.Price);
             }
-            return newOrder;
+
+            var customer = Customer.Create(
+                id: CustomerId.New(orderDto.Customer.CustomerId),
+                name: orderDto.Customer.Name!,
+                nationalCode: orderDto.Customer.nationalCode!,
+                phoneNumber: orderDto.Customer.PhoneNumber!,
+                email: orderDto.Customer.Email!);
+
+            return (newOrder, customer);
         }
     }
 }
