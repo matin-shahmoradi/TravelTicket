088b8a01c18a0f5962e405176de93c072be3765b
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-06-25 19:48:49 +0330

    feat(order) : add Carter and CorrelationId support to API setup
    - Integrated Carter endpoints and CorrelationId middleware into the API.
    - Registered Carter, HttpContextAccessor, and CorrelationId services in DI.
    - Updated the pipeline to use authentication, authorization, and Carter mapping.

--- src/Services/Ordering/Ordering.API/ApiDependencyInjection.cs
+++ src/Services/Ordering/Ordering.API/ApiDependencyInjection.cs
@@ -1,4 +1,6 @@
-﻿using System.Reflection;
+﻿using BuildingBlocks.Infrastracture.CorrelationId;
+using Carter;
+using System.Reflection;
 
 namespace Ordering.API
 {
@@ -6,15 +8,22 @@ namespace Ordering.API
     {
         public static IServiceCollection AddApiService(this IServiceCollection services)
         {
-            services.AddMediatR(cfg => 
+            services.AddMediatR(cfg =>
             {
                 cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
             });
+            services.AddCarter();
+            services.AddHttpContextAccessor();
+            services.AddCorrelationId();
             return services;
         }
 
         public static WebApplication UseApiService(this WebApplication app)
         {
+            app.UseCorrelationId();
+            app.UseAuthentication();
+            app.UseAuthorization();
+            app.MapCarter();
             return app;
         }
     }
