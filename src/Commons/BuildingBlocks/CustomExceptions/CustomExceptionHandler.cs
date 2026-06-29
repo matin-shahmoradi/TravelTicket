667c80829cf7d548e087e65384fe25db56513d23
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-06-29 14:53:02 +0330

    feat(buildingBlock) : add custom exception handler for unauthorized access
    - Introduced CustomExceptionHandler to handle UnAuthorizedException by returning a 401 response with problem details.
    - Added UnAuthorizedException class for representing unauthorized access errors.

---
+++ src/Commons/BuildingBlocks/CustomExceptions/CustomExceptionHandler.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuildingBlocks.CustomExceptions
+{
+    public class CustomExceptionHandler : IExceptionHandler
+    {
+        public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+        {
+            if (exception is UnAuthorizedException unAuthorizedException)
+            {
+                var problemDetails = new ProblemDetails
+                {
+                    Status = StatusCodes.Status401Unauthorized,
+                    Title = "UnAuthorized",
+                    Detail = unAuthorizedException.Message
+                };
+
+                httpContext.Response.StatusCode = problemDetails.Status.Value;
+                await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ src/Commons/BuildingBlocks/CustomExceptions/UnAuthorizedException.cs
@@ -0,0 +1,10 @@
+﻿namespace BuildingBlocks.CustomExceptions
+{
+    public class UnAuthorizedException : Exception
+    {
+        public UnAuthorizedException(string message) : base(message)
+        {
+
+        }
+    }
+}
