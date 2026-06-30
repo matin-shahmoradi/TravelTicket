9ddca59160dc500cc05f57b10a1d5f5c3078c19f
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-06-30 20:25:38 +0330

    refactor(catalog) : refactor CurrentUser claims handling and exceptions
    - Refactored CurrentUser to throw UnAuthorizedException when required claims are missing and made UserId, UserName, and UserEmail non-nullable.
    - Updated claim lookups to use JwtRegisteredClaimNames, adjusted Roles retrieval, and simplified IsInRole logic.
    - Added necessary using directives for custom exceptions.

--- src/Services/Catalog/Catalog.API/Tickets/CurrentUser/CurrentUser.cs
+++ src/Services/Catalog/Catalog.API/Tickets/CurrentUser/CurrentUser.cs
@@ -1,4 +1,6 @@
 ﻿using BuildingBlocks.Abstractions;
+using BuildingBlocks.CustomExceptions;
+using Microsoft.IdentityModel.JsonWebTokens;
 using System.Security.Claims;
 
 namespace Catalog.API.Tickets.CurrentUser
@@ -7,29 +9,36 @@ namespace Catalog.API.Tickets.CurrentUser
         : ICurrentUser
     {
         private ClaimsPrincipal? _principal =>
-            httpContextAccessor.HttpContext?.User;
+           httpContextAccessor.HttpContext?.User;
+        public bool IsAuthenticated => _principal?.Identity?.IsAuthenticated ?? false;
 
-        public Guid? UserId
+        public Guid UserId
         {
             get
             {
-                var value = _principal!.FindFirstValue(ClaimTypes.NameIdentifier);
-                return Guid.TryParse(value, out var userId) ? userId : null;
+                var value = _principal?.FindFirstValue(ClaimTypes.NameIdentifier);
+                return Guid.TryParse(value, out var userId)
+                    ? userId
+                    : throw new UnAuthorizedException("User Id can't be null");
             }
         }
 
-        public string? UserName => _principal!.FindFirst(ClaimTypes.Name)!.Value;
-        public string? UserEmail => _principal!.FindFirst(ClaimTypes.Email)!.Value;
-        public bool IsAuthenticated => _principal?.Identity?.IsAuthenticated ?? false;
-        public IReadOnlyCollection<string> Roles =>
-            _principal?
-                .FindAll(ClaimTypes.Role)
-                .Select(x => x.Value)
-                .ToArray() ?? [];
+        public string UserName =>
+           _principal?.FindFirstValue(JwtRegisteredClaimNames.UniqueName)
+           ?? throw new UnAuthorizedException("Username Id can't be null");
 
-        public bool IsInRole(string role)
-        {
-            return Roles.Contains(role, StringComparer.OrdinalIgnoreCase);
-        }
+        public string UserEmail =>
+            _principal?.FindFirstValue(JwtRegisteredClaimNames.Email)
+            ?? throw new UnAuthorizedException("Email User email");
+
+        public string PhoneNumber =>
+            _principal?.FindFirstValue(JwtRegisteredClaimNames.PhoneNumber)
+            ?? throw new UnAuthorizedException("Phone number User phone number");
+
+        public IReadOnlyCollection<string> Roles =>
+            _principal?.FindAll("role").Select(c => c.Value).ToList().AsReadOnly()
+            ?? new List<string>().AsReadOnly();
+
+        public bool IsInRole(string role) => _principal?.IsInRole(role) ?? false;
     }
 }
