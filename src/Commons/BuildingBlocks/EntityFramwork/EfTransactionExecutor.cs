a186a3b90b51395ba3790468598d87f8483fd818
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-07-09 19:55:25 +0330

    feat(buildingBlcok) : Add ITransactionExecutor and EF implementation
    - Introduced ITransactionExecutor interface for async transactional operations.
    - Added EfTransactionExecutor<TDbContext> to handle EF Core transactions with execution strategy, commit, and rollback support.

---
+++ src/Commons/BuildingBlocks/EntityFramwork/EfTransactionExecutor.cs
@@ -0,0 +1,31 @@
+﻿using BuildingBlocks.Abstractions;
+using Microsoft.EntityFrameworkCore;
+
+namespace BuildingBlocks.EntityFramwork
+{
+    public sealed class EfTransactionExecutor<TDbContext>(TDbContext dbContext) : ITransactionExecutor
+        where TDbContext : DbContext
+    {
+        public async Task<TResult> ExecuteAsync<TResult>(Func<CancellationToken, Task<TResult>> action, CancellationToken cancellationToken = default)
+        {
+            var strategy = dbContext.Database.CreateExecutionStrategy();
+
+            return await strategy.ExecuteAsync(async () =>
+            {
+                await using var transaction = dbContext.Database.BeginTransaction();
+                try
+                {
+                    var result = await action(cancellationToken);
+                    await dbContext.SaveChangesAsync(cancellationToken);
+                    await transaction.CommitAsync(cancellationToken);
+                    return result;
+                }
+                catch (Exception)
+                {
+                    await transaction.RollbackAsync(cancellationToken);
+                    throw;
+                }
+            });
+        }
+    }
+}
