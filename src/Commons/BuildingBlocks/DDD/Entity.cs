02893493dba01b3a33c9a6c5c3a46bf77e014b7f
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-07-09 19:57:28 +0330

    feat(BuildingBlock) : add Version property and Initialize Id with default!
    - Added a Version property of type long to Entity<T> for version tracking.

--- src/Commons/BuildingBlocks/DDD/Entity.cs
+++ src/Commons/BuildingBlocks/DDD/Entity.cs
@@ -2,10 +2,11 @@
 {
     public class Entity<T> : IEntity<T>
     {
-        public T Id { get; set; }
+        public T Id { get; set; } = default!;
         public DateTime? CreatedAt { get; set; }
         public string? CreatedBy { get; set; }
         public DateTime? LastModified { get; set; }
         public string? LastModifiedBy { get; set; }
+        public long Version { get; set; }
     }
 }
