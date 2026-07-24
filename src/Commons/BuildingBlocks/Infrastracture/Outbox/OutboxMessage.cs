3fb1f41c9ab534c1c12a83d177b3921f58fecdce
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-07-24 22:00:52 +0330

    feat(buildingBlock) : Add OutboxMessage entity and EF Core configuration
    - Added OutboxMessage entity with properties for outbox pattern support and a factory method for IntegrationEvent serialization.
    - Introduced OutboxMessageBaseEntityConfiguration for EF Core mapping, including key, property constraints, and column types.

---
+++ src/Commons/BuildingBlocks/Infrastracture/Outbox/OutboxMessage.cs
@@ -0,0 +1,28 @@
+﻿using BuildingBlocks.Messaging.Events;
+using System.Text.Json;
+
+namespace BuildingBlocks.Infrastracture.Outbox
+{
+    public sealed class OutboxMessage
+    {
+        public Guid Id { get; set; }
+        public required string Type { get; set; }
+        public required string Content { get; set; }
+
+        public DateTime OccuredOnUtc { get; set; }
+
+        public DateTime? ProcessedOnUtc { get; set; }
+        public string? Error { get; set; }
+
+        public static OutboxMessage CreateOutboxMessage(IntegrationEvent @event)
+        {
+            return new OutboxMessage
+            {
+                Id = @event.EventId,
+                Type = @event.GetType().AssemblyQualifiedName!,
+                Content = JsonSerializer.Serialize(@event),
+                OccuredOnUtc = @event.OccuredOn,
+            };
+        }
+    }
+}
---
+++ src/Commons/BuildingBlocks/Infrastracture/Outbox/OutboxMessageBaseEntityConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BuildingBlocks.Infrastracture.Outbox
+{
+    public class OutboxMessageBaseEntityConfiguration : IEntityTypeConfiguration<OutboxMessage>
+    {
+        public void Configure(EntityTypeBuilder<OutboxMessage> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Type)
+                .HasMaxLength(255)
+                .IsRequired();
+
+            builder.Property(x => x.Content)
+                .HasColumnName("content")
+                .HasColumnType("JSONB")
+                .IsRequired();
+
+            builder.Property(x => x.OccuredOnUtc)
+                 .IsRequired();
+
+            builder.Property(x => x.ProcessedOnUtc);
+            builder.Property(x => x.Error);
+        }
+    }
+}
