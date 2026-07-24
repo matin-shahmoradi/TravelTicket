b53ff097021c3a128b14d7556bced0c70a4a44e1
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-07-24 16:37:49 +0330

    feat(payment) : add PaymentSucceeded domain event and event handler

---
+++ src/Services/Payment/Payment.api/EventHandlers/PaymentSucceededEventHandler.cs
@@ -0,0 +1,34 @@
+﻿using BuildingBlocks.Messaging.Events.PaymentEvents;
+using MassTransit;
+using MediatR;
+using Payment.api.Domain.Events;
+
+namespace Payment.api.EventHandlers
+{
+    public class PaymentSucceededEventHandler(
+        IBus bus,
+        ILogger<PaymentSucceededEventHandler> logger) : INotificationHandler<PaymentSucceededDomainEvent>
+    {
+        public async Task Handle(PaymentSucceededDomainEvent notification, CancellationToken cancellationToken)
+        {
+            var integrationEvent = new PaymentSucceededIntegrationEvent
+            {
+                OrderId = notification.OrderId,
+                PaymentStatus = notification.PaymentStatus
+            };
+
+            try
+            {
+                await bus.Publish(integrationEvent, cancellationToken);
+                logger.LogInformation("[EVENT] {event} Published", notification.GetType().Name);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(
+                    ex,
+                    "[Event] Failed to handle {event}",
+                    notification.GetType().Name);
+            }
+        }
+    }
+}
