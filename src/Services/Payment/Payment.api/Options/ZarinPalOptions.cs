fdf432e05278eb3beb2cd1ac96d0f73f8d70d0a5
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-07-24 16:53:54 +0330

    feat(payment) : Add ZarinPal options class and config files
    Added ZarinPalOptions class for payment settings. Introduced appsettings.json and appsettings.Development.json with logging, connection strings, message broker, and ZarinPal configuration sections.

---
+++ src/Services/Payment/Payment.api/Options/ZarinPalOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace Payment.api.Options
+{
+    public class ZarinPalOptions
+    {
+        public string MerchantId { get; init; } = default!;
+        public string RequestUrl { get; init; } = default!;
+        public string VerifyUrl { get; init; } = default!;
+        public string PaymentPageUrl { get; init; } = default!;
+        public string CallbackUrl { get; init; } = default!;
+    }
+}
