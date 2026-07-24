87c088b0cf2fea3913ab584a4234308f99fc26a0
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-07-24 16:44:04 +0330

    feat(payment): Add GetPaymentUrlQuery and handler for payment URL retrieval
    - Implemented GetPaymentUrlQuery and its validator to fetch payment URLs by OrderId.
    - Added GetPaymentUrlQueryHandler to retrieve payment info from the repository, construct the ZarinPal payment URL, and return appropriate results based on payment existence.

---
+++ src/Services/Payment/Payment.api/Features/GetPaymentById/GetPaymentUrlQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using BuildingBlocks;
+using BuildingBlocks.CQRS;
+using Microsoft.Extensions.Options;
+using Payment.api.Domain.DTOs;
+using Payment.api.Options;
+using Payment.api.Repositories;
+
+namespace Payment.api.Features.GetPaymentById
+{
+    internal sealed class GetPaymentUrlQueryHandler(
+        IPaymentRepository paymentRepository,
+        IOptions<ZarinPalOptions> zarinPalOption)
+        : IQueryHandler<GetPaymentUrlQuery, Result<ZarinPalRequestResult>>
+    {
+        public async Task<Result<ZarinPalRequestResult>> Handle(GetPaymentUrlQuery query, CancellationToken cancellationToken)
+        {
+            var payment = await paymentRepository.GetByOrderIdAsync(query.OrderId);
+
+            if (payment is null)
+                return Result<ZarinPalRequestResult>.Failure(Error.NotFoundError($"Payment with id {query.OrderId} not found."));
+
+            string payment_url = $"{zarinPalOption.Value.PaymentPageUrl}{payment?.Authority}";
+
+            var result = new ZarinPalRequestResult(true, payment!.Authority, payment_url, 100, null);
+            return Result<ZarinPalRequestResult>.Success(result);
+        }
+    }
+}
