f1f90fa5c348323946d7d3268345c2ade9178028
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-07-24 22:04:52 +0330

    feat(buildingBlock) : add OutboxExtensions for Hangfire and event integration
    Introduced OutboxExtensions with methods to configure Hangfire using PostgreSQL and to add integration events to the OutboxMessage DbSet. Includes event serialization, logging, and server polling interval setup.

---
+++ src/Commons/BuildingBlocks/Infrastracture/Outbox/Extensions/OutboxExtensions.cs
@@ -0,0 +1,47 @@
+﻿using BuildingBlocks.Messaging;
+using Hangfire;
+using Hangfire.PostgreSql;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Text.Json;
+
+namespace BuildingBlocks.Infrastracture.Outbox.Extensions
+{
+    public static class OutboxExtensions
+    {
+        public static IServiceCollection OutboxServices(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddHangfire(cfg =>
+                cfg.UsePostgreSqlStorage(
+                    options => options.UseNpgsqlConnection(configuration.GetConnectionString("DefaultConnection"))));
+
+            services.AddHangfireServer(options => options.SchedulePollingInterval = TimeSpan.FromSeconds(1));
+            return services;
+        }
+
+        public static void AddIntegrationEvent(
+            this DbSet<OutboxMessage> outboxMessages,
+            IIntegrationEvent integrationEvent)
+        {
+            ArgumentNullException.ThrowIfNull(integrationEvent);
+
+            var messageType = integrationEvent.GetType();
+            Console.WriteLine($"[OUTBOX-INTEGRATION] {messageType.AssemblyQualifiedName}");
+            var message = new OutboxMessage
+            {
+                Id = integrationEvent.EventId,
+                Type = messageType.AssemblyQualifiedName!,
+                Content = JsonSerializer.Serialize(integrationEvent, messageType),
+                OccuredOnUtc = integrationEvent.OccuredOn,
+            };
+
+            outboxMessages.Add(message);
+
+            Console.WriteLine($"[OUTBOX-CREATED] {message.Type}");
+            Console.WriteLine(Environment.StackTrace);
+        }
+    }
+}
