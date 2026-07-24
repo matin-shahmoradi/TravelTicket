fe260c730903ce2281db935dc0190be082994cd1
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-07-24 22:06:05 +0330

    refactor(buildingBlock) : refactor IntegrationEvent to abstract class with interface
    Refactored IntegrationEvent to be an abstract class implementing the new IIntegrationEvent interface. Changed EventId and OccuredOn to init-only auto-properties. Introduced IIntegrationEvent to define the event contract.

--- src/Commons/BuildingBlocks/Messaging/Events/IntegrationEvent.cs
+++ src/Commons/BuildingBlocks/Messaging/Events/IntegrationEvent.cs
@@ -1,10 +1,9 @@
 ﻿namespace BuildingBlocks.Messaging.Events
 {
-    public class IntegrationEvent
+    public abstract class IntegrationEvent : IIntegrationEvent
     {
-        public Guid EventId => Guid.NewGuid();
-        public DateTime OccuredOn => DateTime.UtcNow;
+        public Guid EventId { get; init; } = Guid.NewGuid();
+        public DateTime OccuredOn { get; init; } = DateTime.UtcNow;
         public string EventType => GetType().AssemblyQualifiedName!;
-
     }
 }
---
+++ src/Commons/BuildingBlocks/Messaging/IIntegrationEvent.cs
@@ -0,0 +1,10 @@
+﻿namespace BuildingBlocks.Messaging
+{
+    public interface IIntegrationEvent
+    {
+        public Guid EventId { get; }
+        public DateTime OccuredOn { get; }
+        public string EventType { get; }
+    };
+
+}
