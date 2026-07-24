f472f7261c946a8796a9375c0c7a2164afa4af51
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-07-24 16:55:12 +0330

    feat(payment) : add MigrateDb extension for async EF Core migrations
    Introduced MigrateDb static class with a MigrateDatabase extension method for WebApplication. This enables automatic application of pending EF Core migrations for PaymentDbContext at startup.

---
+++ src/Services/Payment/Payment.api/Data/MigrateDb.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Payment.api.Data
+{
+    public static class MigrateDb
+    {
+        public async static Task MigrateDatabase(this WebApplication app)
+        {
+            using var scoped = app.Services.CreateScope();
+
+            var context = scoped.ServiceProvider.GetRequiredService<PaymentDbContext>();
+
+            await context.Database.MigrateAsync();
+        }
+    }
+}
