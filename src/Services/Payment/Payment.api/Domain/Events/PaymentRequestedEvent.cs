842c7a6f77850652101bdf53be857fd5fdf33aa6
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-07-24 16:39:33 +0330

    feat(payment) : add PaymentRequestedEvent and event handler

---
+++ src/Services/Payment/Payment.api/Domain/Events/PaymentRequestedEvent.cs
@@ -0,0 +1,10 @@
+﻿using BuildingBlocks.DDD;
+
+namespace Payment.api.Domain.Events
+{
+    public record PaymentRequestedEvent(
+        Guid PaymentId,
+        string Authority,
+        string Status) : IDomainEvent;
+
+}
