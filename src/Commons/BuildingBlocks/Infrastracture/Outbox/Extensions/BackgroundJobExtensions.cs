a2e9a8dd43cf11cbb67e814c86be118660bdecf2
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-07-24 22:05:16 +0330

    feat(buildingBlock) : add extension for recurring Hangfire outbox job setup
    Introduced BackgroundJobExtensions with a UseBackgroundJobs extension method for WebApplication. This registers a recurring Hangfire job to execute IOutboxProcessor.ProcessMessageAsync() every minute, using a specified jobId.

---
+++ src/Commons/BuildingBlocks/Infrastracture/Outbox/Extensions/BackgroundJobExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Hangfire;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BuildingBlocks.Infrastracture.Outbox.Extensions
+{
+    public static class BackgroundJobExtensions
+    {
+        public static IApplicationBuilder UseBackgroundJobs(this WebApplication app, string jobId)
+        {
+            app.Services
+                .GetRequiredService<IRecurringJobManager>()
+                .AddOrUpdate<IOutboxProcessor>(
+                    jobId,
+                    job => job.ProcessMessageAsync(),
+                    Cron.Minutely());
+            return app;
+        }
+    }
+}
