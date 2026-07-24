cfb073e4b80566b96b843e50f4ecc86e409392c9
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-07-24 16:49:37 +0330

    feat(payment) : add OrderPaymentRequestedConsumer for payment requests
    Introduced OrderPaymentRequestedConsumer to handle OrderCreatedIntegrationEvent messages. The consumer creates and sends a CreatePaymentCommand using MediatR and includes logging for both success and error scenarios.

---
+++ src/Services/Payment/Payment.api/Consumers/OrderPaymentRequestedConsumer.cs
@@ -0,0 +1,36 @@
+﻿using BuildingBlocks.Messaging.Events.OrderEvents;
+using MassTransit;
+using MediatR;
+using Payment.api.Features.PaymentRequest;
+
+namespace Payment.api.Consumers
+{
+    public sealed class OrderPaymentRequestedConsumer(
+        ISender sender,
+        ILogger<OrderPaymentRequestedConsumer> logger)
+        : IConsumer<OrderCreatedIntegrationEvent>
+    {
+        public async Task Consume(ConsumeContext<OrderCreatedIntegrationEvent> context)
+        {
+            var command = new CreatePaymentCommand(
+                OrderId: context.Message.OrderId,
+                CustomerId: context.Message.CustomerId,
+                Amount: context.Message.Amount);
+
+            try
+            {
+                var send = await sender.Send(command);
+                logger.LogInformation(
+                    "[PaymentRequestCommandHandler] consumed {context}",
+                     context.Message.GetType().Name);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(
+                   exception: ex,
+                   message: "[Handler] Failed to handle {command}",
+                    command.GetType().Name);
+            }
+        }
+    }
+}
