a3c6cdbd76bcc523306d2345f6255e9030b0e565
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-07-24 16:46:08 +0330

    feat(payment) : add payment repository interface and implementation
    Introduced IPaymentRepository for managing PaymentModel entities.
    Implemented PaymentRepository using EF Core for async CRUD operations,
    including retrieval by ID, order ID, authority, and methods to add and
    persist payments.

---
+++ src/Services/Payment/Payment.api/Repositories/IPaymentRepository.cs
@@ -0,0 +1,15 @@
+﻿using Payment.api.Domain;
+
+namespace Payment.api.Repositories
+{
+    public interface IPaymentRepository
+    {
+        Task<PaymentModel?> GetByIdAsync(
+            Guid paymentId,
+            CancellationToken cancellationToken = default);
+        Task<PaymentModel?> GetByOrderIdAsync(Guid orderId, CancellationToken cancellationToken = default);
+        Task<PaymentModel?> GetByAuthorityAsync(string authority, CancellationToken cancellationToken);
+        Task AddAsync(PaymentModel payment, CancellationToken cancellationToken);
+        Task SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
---
+++ src/Services/Payment/Payment.api/Repositories/PaymentRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Payment.api.Data;
+using Payment.api.Domain;
+using Payment.api.Domain.StrongIdTypes;
+
+namespace Payment.api.Repositories
+{
+    internal sealed class PaymentRepository(PaymentDbContext dbContext) : IPaymentRepository
+    {
+        public async Task<PaymentModel?> GetByIdAsync(Guid paymentId, CancellationToken cancellationToken)
+        {
+            return await dbContext.Payments.FindAsync(PaymentId.Of(paymentId), cancellationToken);
+        }
+
+        public async Task<PaymentModel?> GetByOrderIdAsync(Guid orderId, CancellationToken cancellationToken)
+        {
+            return await dbContext.Payments.SingleOrDefaultAsync(x => x.OrderId == OrderId.Of(orderId));
+            //return dbContext.Payments
+            //    .FirstOrDefaultAsync(payment => payment.OrderId == OrderId.Of(orderId), cancellationToken);
+        }
+
+        public Task<PaymentModel?> GetByAuthorityAsync(string authority, CancellationToken cancellationToken)
+        {
+            return dbContext.Payments
+                .FirstOrDefaultAsync(payment => payment.Authority == authority, cancellationToken);
+        }
+
+        public async Task AddAsync(PaymentModel payment, CancellationToken cancellationToken) =>
+            await dbContext.Payments.AddAsync(payment, cancellationToken);
+        public async Task SaveChangesAsync(CancellationToken cancellationToken) =>
+            await dbContext.SaveChangesAsync(cancellationToken);
+    }
+}
