5a5f4943d9749f62f9455bae47d10918f330e2d5
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-07-24 22:07:42 +0330

    feat(buildingBlock) : add payment  integration events
    Introduced PaymentSucceededIntegrationEvent and PaymentFailedIntegrationEvent classes in the PaymentEvents namespace. Both events inherit from IntegrationEvent and include OrderId and PaymentStatus properties to represent payment outcomes.

---
+++ src/Commons/BuildingBlocks/Messaging/Events/PaymentEvents/PaymentFailedIntegrationEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace BuildingBlocks.Messaging.Events.PaymentEvents
+{
+    public sealed class PaymentFailedIntegrationEvent : IntegrationEvent
+    {
+        public Guid OrderId { get; init; }
+        public string PaymentStatus { get; init; } = default!;
+    }
+}
---
+++ src/Commons/BuildingBlocks/Messaging/Events/PaymentEvents/PaymentSucceededIntegrationEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace BuildingBlocks.Messaging.Events.PaymentEvents
+{
+    public sealed class PaymentSucceededIntegrationEvent : IntegrationEvent
+    {
+        public Guid OrderId { get; init; }
+        public string PaymentStatus { get; init; } = default!;
+    }
+}
