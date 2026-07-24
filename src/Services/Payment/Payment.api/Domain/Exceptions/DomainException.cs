a81b41309100a65d158fa324fa6cf4e5e2a626be
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-07-24 16:54:19 +0330

    feat(payment) : Add DomainException custom exception class
    Created DomainException.cs with a custom exception class in the Payment.api.Domain.Exceptions namespace. This class extends Exception and provides a constructor for custom error messages.

---
+++ src/Services/Payment/Payment.api/Domain/Exceptions/DomainException.cs
@@ -0,0 +1,10 @@
+﻿namespace Payment.api.Domain.Exceptions
+{
+    public class DomainException : Exception
+    {
+        public DomainException(string message) : base(message)
+        {
+
+        }
+    }
+}
