05c427db6cf5f78a263221713bbd3d60c4858d45
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-07-24 21:59:30 +0330

    feat(buildingblock) : Add generic UnitOfWork implementation for DbContext
    Introduced UnitOfWork class in BuildingBlocks.EntityFramwork, implementing IUnitOfWork for generic DbContext. Added async SaveChangesAsync method and necessary using directives.

---
+++ src/Commons/BuildingBlocks/EntityFramwork/UnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using Basket.API.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace BuildingBlocks.EntityFramwork
+{
+    public sealed class UnitOfWork<TContext>(TContext context) : IUnitOfWork
+        where TContext : DbContext
+    {
+        public async Task SaveChangesAsync(CancellationToken cancellationToken)
+        {
+            await context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
