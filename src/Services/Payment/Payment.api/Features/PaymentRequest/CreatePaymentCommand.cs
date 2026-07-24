1d989065b394f1fc6b5a329dcc87b5adf5a46674
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-07-24 16:41:18 +0330

    feat(payment) : Add payment request feature with command and handler

---
+++ src/Services/Payment/Payment.api/Features/PaymentRequest/CreatePaymentCommand.cs
@@ -0,0 +1,29 @@
+﻿using BuildingBlocks;
+using BuildingBlocks.CQRS;
+using FluentValidation;
+
+namespace Payment.api.Features.PaymentRequest
+{
+    public sealed record RequestPaymentResult(Guid PaymentId, string Authority, string PaymentUrl, int Status);
+    public sealed record CreatePaymentCommand(
+         Guid OrderId,
+         Guid CustomerId,
+         decimal Amount) : ICommand<Result<RequestPaymentResult>>;
+
+    public class PaymentRequestCommandValidator : AbstractValidator<CreatePaymentCommand>
+    {
+        public PaymentRequestCommandValidator()
+        {
+            RuleFor(x => x.OrderId)
+                .NotEmpty().WithMessage("Order is required");
+
+            RuleFor(x => x.CustomerId)
+                .NotEmpty().WithMessage("Customer id is required");
+
+            RuleFor(x => x.Amount)
+                .NotEmpty().WithMessage("Amount is required")
+                .GreaterThanOrEqualTo(0).WithMessage("Amount cant be negative number");
+        }
+    }
+
+}
