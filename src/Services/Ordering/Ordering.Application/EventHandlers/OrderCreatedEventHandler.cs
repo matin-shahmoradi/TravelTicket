512f766816a3bdac4da91fa98748c432e67aeee7
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-07-26 17:43:41 +0330

    feat(order) : add OrderCreatedEventHandler for outbox integration
    Introduced OrderCreatedEventHandler to handle OrderCreatedEvent notifications. The handler creates and adds OrderCreatedIntegrationEvent to the outbox

---
+++ src/Services/Ordering/Ordering.Application/EventHandlers/OrderCreatedEventHandler.cs
@@ -0,0 +1,38 @@
+﻿using BuildingBlocks.Infrastracture.Outbox.Extensions;
+using BuildingBlocks.Logger;
+using BuildingBlocks.Messaging.Events.OrderEvents;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Domain.Events;
+
+namespace Ordering.Application.EventHandlers
+{
+    internal sealed class OrderCreatedEventHandler(
+        IOrderDbContext orderDbContext,
+        ILogger<OrderCreatedEventHandler> logger)
+        : INotificationHandler<OrderCreatedEvent>
+    {
+        public async Task Handle(OrderCreatedEvent notification, CancellationToken cancellationToken)
+        {
+            var integrationEvent = new OrderCreatedIntegrationEvent
+            {
+                OrderId = notification.Order.Id.Value,
+                CustomerId = notification.Order.CustomerId.Value,
+                Amount = notification.Order.TotalPrice
+            };
+            try
+            {
+                orderDbContext.OutboxMessages.AddIntegrationEvent(integrationEvent);
+                await orderDbContext.SaveChangesAsync(cancellationToken);
+                logger.LogEventHandler(integrationEvent.GetType().Name);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(
+                    ex,
+                    "[EVENT] Failed to handle {event}",
+                    integrationEvent.GetType().Name);
+            }
+        }
+    }
+}
