6063142fdc522cbd6974b162df136d38e03bc9e8
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-07-26 19:14:43 +0330

    feat(order) : add payment event consumers for order status updates
    Added PaymentFailedEventConsumer and PaymentSucceededEventConsumer to handle payment integration events. These consumers log received events, update order status via MediatR commands, and include exception handling and logging.

---
+++ src/Services/Ordering/Ordering.Infrastructure/Consumers/PaymentFailedEventConsumer.cs
@@ -0,0 +1,36 @@
+﻿using BuildingBlocks.Logger;
+using BuildingBlocks.Messaging.Events.PaymentEvents;
+using MassTransit;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Dtos;
+using Ordering.Application.Orders.Commands.UpdateOrder;
+
+namespace Ordering.Infrastructure.Consumers
+{
+    public sealed class PaymentFailedEventConsumer(
+        ISender sender,
+        ILogger<PaymentFailedEventConsumer> logger)
+        : IConsumer<PaymentFailedIntegrationEvent>
+    {
+        public async Task Consume(ConsumeContext<PaymentFailedIntegrationEvent> context)
+        {
+            logger.LogEventReceived(context.MessageId, nameof(PaymentFailedEventConsumer));
+            var orderDto = new UpdateOrderStatusDto(context.Message.OrderId, context.Message.PaymentStatus);
+            var command = new UpdateOrderCommand(orderDto);
+
+            try
+            {
+                await sender.Send(command);
+                logger.LogEventConsumed(context.MessageId, nameof(PaymentFailedEventConsumer));
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(
+                    ex,
+                    "[EVENT HANDLER] Failed to handle event : {event}",
+                    context.MessageId);
+            }
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Infrastructure/Consumers/PaymentSucceededEventConsumer.cs
@@ -0,0 +1,36 @@
+﻿using BuildingBlocks.Logger;
+using BuildingBlocks.Messaging.Events.PaymentEvents;
+using MassTransit;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Dtos;
+using Ordering.Application.Orders.Commands.UpdateOrder;
+
+namespace Ordering.Infrastructure.Consumers
+{
+    public sealed class PaymentSucceededEventConsumer(
+        ISender sender,
+        ILogger<PaymentSucceededEventConsumer> logger)
+        : IConsumer<PaymentSucceededIntegrationEvent>
+    {
+        public async Task Consume(ConsumeContext<PaymentSucceededIntegrationEvent> context)
+        {
+            logger.LogEventReceived(context.MessageId, nameof(PaymentSucceededEventConsumer));
+            var orderDto = new UpdateOrderStatusDto(
+                context.Message.OrderId,
+                context.Message.PaymentStatus);
+
+            var command = new UpdateOrderCommand(orderDto);
+            try
+            {
+                await sender.Send(command);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex,
+                    "failed to handle {event}", context.GetType().Name);
+            }
+
+        }
+    }
+}
