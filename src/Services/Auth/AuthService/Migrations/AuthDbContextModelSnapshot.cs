e197d7bab885a5efcc3194021349bdf2e17891df
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-07-26 19:20:13 +0330

    build(auth)  :Update EF Core model snapshot to version 10.0.9
    Updated the ProductVersion annotation in AuthDbContextModelSnapshot.cs
    from "10.0.8" to "10.0.9" to reflect the new database provider version.

--- src/Services/Auth/AuthService/Migrations/AuthDbContextModelSnapshot.cs
+++ src/Services/Auth/AuthService/Migrations/AuthDbContextModelSnapshot.cs
@@ -17,7 +17,7 @@ namespace AuthService.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "10.0.8")
+                .HasAnnotation("ProductVersion", "10.0.9")
                 .HasAnnotation("Relational:MaxIdentifierLength", 63);
 
             NpgsqlModelBuilderExtensions.UseIdentityByDefaultColumns(modelBuilder);
