9e1fb63ede9a412960e30b01303cdb92f3c48d4a
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-07-26 17:59:41 +0330

    feat(buildingBlock) : add LoggerExtensions with event logging methods
    Introduced LoggerExtensions static class in BuildingBlocks.Logger, adding LogEventHandler, LogEventReceived, and LogEventConsumed extension methods for ILogger to standardize structured logging of event handling and consumption.

---
+++ src/Commons/BuildingBlocks/Logger/LoggerExtensions.cs
@@ -0,0 +1,33 @@
+﻿namespace BuildingBlocks.Logger
+{
+    public static class LoggerExtensions
+    {
+        public static void LogEventHandler(
+            this ILogger logger,
+            string eventName)
+        {
+            logger.LogInformation("[EVENT HANDLER] {eventName} Handled successfully", eventName);
+        }
+        public static void LogEventReceived(
+            this ILogger logger,
+            Guid? messageId,
+            string consumerName)
+        {
+            logger.LogInformation(
+                "[INTEGRATION EVENT] RECEIVED | MessageId : {messageId}, Consumer : {consumerName}",
+                messageId,
+                consumerName);
+        }
+
+        public static void LogEventConsumed(
+            this ILogger logger,
+            Guid? messageId,
+            string consumerName)
+        {
+            logger.LogInformation(
+               "[EVENT CONSUMER] | MessageId : {messageId} consumed by : {consumerName}",
+               messageId,
+               consumerName);
+        }
+    }
+}
