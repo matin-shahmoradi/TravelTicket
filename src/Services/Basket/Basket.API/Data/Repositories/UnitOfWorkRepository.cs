a38996d7d84c5f2600a3df4a833efd599127972a
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-07-26 16:52:55 +0330

    refactor : Remove UnitOfWorkRepository and related implementation
    Deleted UnitOfWorkRepository.cs, which contained the UnitOfWorkRepository class implementing IUnitOfWork with SaveChangesAsync using BacketDbContext. This refactoring removes unused infrastructure.

--- src/Services/Basket/Basket.API/Data/Repositories/UnitOfWorkRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Basket.API.Data.Repositories
-{
-    public sealed class UnitOfWorkRepository(BacketDbContext basketDbContext) : IUnitOfWork
-    {
-        public async Task SaveChangesAsync(CancellationToken cancellation) =>
-            await basketDbContext.SaveChangesAsync(cancellation);
-    }
-}
