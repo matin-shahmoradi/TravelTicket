b2c000aa3cae8c275eae87ba20f38a32fc28b140
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-08-03 18:52:22 +0330

    feat(catalog) : add TicketQueryRepository and interface for ticket queries
    - Introduced ITicketQueryRepository interface with methods to retrieve Ticket entities by ID, both with and without tracking.
    - Implemented the interface in TicketQueryRepository using CatalogDbContext.
    - Added GetTicketById and GetTicketByIdWithNoTracking methods.

---
+++ src/Services/Catalog/Catalog.API/Repository/ITicketQueryRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Catalog.API.Repository
+{
+    public interface ITicketQueryRepository
+    {
+        Task<Ticket?> GetTicketById(
+            Guid id,
+            CancellationToken cancellationToken = default);
+
+        Task<Ticket?> GetTicketByIdWithNoTracking(Guid id, CancellationToken cancellationToken = default);
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Repository/TicketQueryRepository.cs
@@ -0,0 +1,18 @@
+﻿using Catalog.API.Domain.ValueObjects;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.API.Repository
+{
+    public class TicketQueryRepository(CatalogDbContext context) : ITicketQueryRepository
+    {
+        public async Task<Ticket?> GetTicketById(Guid id, CancellationToken cancellationToken = default)
+        {
+            return await context.Tickets.FindAsync(id, cancellationToken);
+        }
+
+        public async Task<Ticket?> GetTicketByIdWithNoTracking(Guid id, CancellationToken cancellationToken = default)
+        {
+            return await context.Tickets.SingleOrDefaultAsync(x => x.Id == TicketId.New(id), cancellationToken);
+        }
+    }
+}
