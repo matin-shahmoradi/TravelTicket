275235e7fcf7b8d32255a379a122dd5255f30839
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-08-03 18:43:28 +0330

    feat(catalog): Add validation and update methods to Ticket entity
    Added input validation to the Ticket constructor to enforce non-empty and valid values. Refactored the Update method for clarity and correct event raising. Introduced property-specific change methods with validation and domain event handling.

--- src/Services/Catalog/Catalog.API/Domain/Ticket.cs
+++ src/Services/Catalog/Catalog.API/Domain/Ticket.cs
@@ -1,4 +1,5 @@
-﻿using Catalog.API.Domain.ValueObjects;
+﻿using Catalog.API.Domain.Exception;
+using Catalog.API.Domain.ValueObjects;
 
 namespace Catalog.API.Models
 {
@@ -18,6 +19,26 @@ namespace Catalog.API.Models
             DateTime travelDate,
             decimal price)
         {
+            if (string.IsNullOrWhiteSpace(origin))
+            {
+                throw new DomainException("Origin cant be null or empty");
+            }
+            if (string.IsNullOrWhiteSpace(destination))
+            {
+                throw new DomainException("destination cant be null or empty");
+            }
+            if (string.IsNullOrWhiteSpace(description))
+            {
+                throw new DomainException("description cant be null or empty");
+            }
+            if (origin.Equals(destination, StringComparison.OrdinalIgnoreCase))
+            {
+                throw new DomainException("origin and destination can't be the same");
+            }
+            if (price < 0)
+            {
+                throw new DomainException("price should be greater than zero");
+            }
             var ticket = new Ticket
             {
                 Id = id,
@@ -38,19 +59,80 @@ namespace Catalog.API.Models
             DateTime travelDate,
             decimal price)
         {
+            var priceChanged = this.Price != price;
 
-            Origin = origin;
-            Destination = destination;
-            Description = description;
-            TravelDate = travelDate;
-            Price = price;
+            this.Origin = origin;
+            this.Destination = destination;
+            this.Description = description;
+            this.TravelDate = travelDate;
+            this.Price = price;
 
             AddDomainEvents(new TicketUpdatedEvent(this));
 
-            if(Price != price)
+            if (priceChanged)
             {
                 AddDomainEvents(new TicketPriceChangedEvent(this));
             }
+        }
+
+        public void ChangeOrigin(string newOrigin)
+        {
+            if (string.IsNullOrWhiteSpace(newOrigin))
+                throw new DomainException("new origin can't be empty");
+
+            if (this.Origin == newOrigin)
+                return;
+
+            this.Origin = newOrigin;
+
+            AddDomainEvents(new TicketUpdatedEvent(this));
+        }
+        public void ChangeDestination(string newDestination)
+        {
+            if (string.IsNullOrWhiteSpace(newDestination))
+                throw new DomainException("new Destination can't be empty");
+
+            if (this.Destination == newDestination)
+                return;
+
+            this.Destination = newDestination;
+
+            AddDomainEvents(new TicketUpdatedEvent(this));
+        }
+
+        public void ChangeDescription(string newDescription)
+        {
+            if (string.IsNullOrWhiteSpace(newDescription))
+                throw new DomainException("new Description can't be empty");
+
+            if (this.Description == newDescription)
+                return;
+
+            this.Description = newDescription;
+
+            AddDomainEvents(new TicketUpdatedEvent(this));
+        }
+
+        public void ChangeTravelDate(DateTime newTravelDate)
+        {
+            if (TravelDate == newTravelDate)
+                return;
+
+            this.TravelDate = newTravelDate;
+
+            AddDomainEvents(new TicketUpdatedEvent(this));
+        }
+
+        public void ChangePrice(decimal newPrice)
+        {
+            if (decimal.IsNegative(newPrice))
+                throw new DomainException("price can't be less than zero");
+
+            if (this.Price == newPrice) return;
+
+            this.Price = newPrice;
+
+            AddDomainEvents(new TicketPriceChangedEvent(this));
         }
     }
 }
