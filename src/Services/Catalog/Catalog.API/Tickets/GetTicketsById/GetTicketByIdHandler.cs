5c768f96973f4aad141f7202c0193fc4032bda91
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-08-03 18:58:50 +0330

    feat(catalog) : refactor GetTicketById handler
    Refactored GetTicketByIdQueryHandler to use ITicketQueryRepository and improved error handling with standardized NotFoundError.

--- src/Services/Catalog/Catalog.API/Tickets/GetTicketsById/GetTicketByIdHandler.cs
+++ src/Services/Catalog/Catalog.API/Tickets/GetTicketsById/GetTicketByIdHandler.cs
@@ -1,17 +1,29 @@
-﻿using Catalog.API.Domain.ValueObjects;
+﻿using Catalog.API.Repository;
 
 namespace Catalog.API.Tickets.GetTicketsById
 {
     public record GetTicketByIdQuery(Guid Id) : IQuery<Result<Ticket>>;
-    internal sealed class GetTicketByIdQueryHandler(ICatalogDbContext CatalogDb)
+    public sealed class GetTicketByIdQueryValidator : AbstractValidator<GetTicketByIdQuery>
+    {
+        public GetTicketByIdQueryValidator()
+        {
+            RuleFor(x => x.Id)
+                .NotEmpty().WithMessage("Ticket id is required")
+                .NotEqual(Guid.Empty).WithMessage("Invalid guid");
+        }
+    }
+    internal sealed class GetTicketByIdQueryHandler(ITicketQueryRepository queryRepository)
         : IQueryHandler<GetTicketByIdQuery, Result<Ticket>>
     {
         public async Task<Result<Ticket>> Handle(GetTicketByIdQuery query, CancellationToken cancellationToken)
         {
-            var ticket = await CatalogDb.Tickets.FindAsync(TicketId.New(query.Id),cancellationToken);
-            if(ticket is null)
+            var ticket = await queryRepository.GetTicketByIdWithNoTracking(
+                id: query.Id,
+                cancellationToken);
+
+            if (ticket is null)
             {
-                return Result<Ticket>.Failure(Error.CustomError("Ticket Not found!",404,ErrorType.NOT_FOUND));
+                return Result<Ticket>.Failure(Error.NotFoundError(message: $"Ticket {query.Id} Not Found!"));
             }
             return Result<Ticket>.Success(ticket);
         }
