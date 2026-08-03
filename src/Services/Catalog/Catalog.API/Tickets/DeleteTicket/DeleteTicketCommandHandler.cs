0f7691e0dd80b5670a874588d857df95fd5e865b
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-08-03 18:55:54 +0330

    refactor(catalog) : Refactor DeleteTicket handler and add validation
    - Refactored DeleteTicketCommandHandler to use repository and unit of work patterns. - Added DeleteTicketCommandValidator for command validation.
    - Improved error handling with try-catch and delegated data operations to repositories.

--- src/Services/Catalog/Catalog.API/Tickets/DeleteTicket/DeleteTicketCommandHandler.cs
+++ src/Services/Catalog/Catalog.API/Tickets/DeleteTicket/DeleteTicketCommandHandler.cs
@@ -1,23 +1,43 @@
-﻿using Catalog.API.Domain.ValueObjects;
+﻿using BuildingBlocks.Abstractions;
+using Catalog.API.Repository;
 
 namespace Catalog.API.Tickets.DeleteTicket
 {
     public record DeleteTicketCommand(Guid Id) : ICommand<Result<bool>>;
-    internal sealed class DeleteTicketCommandHandler(ICatalogDbContext catalogDb)
+    public sealed class DeleteTicketCommandValidator : AbstractValidator<DeleteTicketCommand>
+    {
+        public DeleteTicketCommandValidator()
+        {
+            RuleFor(x => x.Id)
+                .NotEmpty().WithMessage("Ticket id is required")
+                .NotEqual(Guid.Empty).WithMessage("Invalid guid");
+        }
+    }
+    internal sealed class DeleteTicketCommandHandler(
+        ITicketQueryRepository queryRepository,
+        ITicketCommandRepository commandRepository,
+        IUnitOfWork uow)
         : ICommandHandler<DeleteTicketCommand, Result<bool>>
     {
         public async Task<Result<bool>> Handle(DeleteTicketCommand command, CancellationToken cancellationToken)
         {
-            var existTicket = await catalogDb.Tickets.FindAsync(TicketId.New(command.Id),cancellationToken);
+            var existTicket = await queryRepository.GetTicketById(command.Id);
             if (existTicket is null)
             {
-                return Result<bool>.Failure(Error.NotFoundError(message:"Ticket Not Found!"));
+                return Result<bool>.Failure(Error.NotFoundError(message: "Ticket Not Found!"));
+            }
+            try
+            {
+                commandRepository.DeleteTicket(existTicket);
+                await uow.SaveChangesAsync(cancellationToken);
+
+                return Result<bool>.Success(true);
+            }
+            catch (Exception ex)
+            {
+                return Result<bool>.Failure(Error.Internal_Server(message: ex.Message));
             }
 
-            catalogDb.Tickets.Remove(existTicket);
-            await catalogDb.SaveChangesAsync(cancellationToken);
-
-            return Result<bool>.Success(true);
         }
     }
 }
