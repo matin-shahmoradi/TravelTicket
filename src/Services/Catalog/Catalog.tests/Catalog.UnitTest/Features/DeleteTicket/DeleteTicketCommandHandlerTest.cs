b8f6e1536b93b419d3fde500b5d63c206630ddf1
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-08-03 19:04:49 +0330

    test(catalog) : add unit tests for DeleteTicket handler and validator
    - Added DeleteTicketCommandHandlerTest with tests for success, not found, and exception scenarios.
    
    - Added DeleteTicketCommandValidatorTest to verify ticket ID validation logic.

---
+++ src/Services/Catalog/Catalog.tests/Catalog.UnitTest/Features/DeleteTicket/DeleteTicketCommandHandlerTest.cs
@@ -0,0 +1,202 @@
+﻿using BuildingBlocks;
+using BuildingBlocks.Abstractions;
+using Catalog.API.Domain.ValueObjects;
+using Catalog.API.Models;
+using Catalog.API.Repository;
+using Catalog.API.Tickets.DeleteTicket;
+using FluentAssertions;
+using Moq;
+
+namespace Catalog.Unit.test.Features.DeleteTicket
+{
+    public class DeleteTicketCommandHandlerTest
+    {
+        private readonly Mock<ITicketQueryRepository> _mockTicketQueryRepository;
+        private readonly Mock<ITicketCommandRepository> _mockTicketCommandRepository;
+        private readonly Mock<IUnitOfWork> _mockUnitOfWork;
+        private readonly DeleteTicketCommandHandler _handler;
+        private readonly CancellationToken _cancellationToken;
+
+        public DeleteTicketCommandHandlerTest()
+        {
+            _mockTicketQueryRepository = new Mock<ITicketQueryRepository>();
+            _mockTicketCommandRepository = new Mock<ITicketCommandRepository>();
+            _mockUnitOfWork = new Mock<IUnitOfWork>();
+            _handler = new DeleteTicketCommandHandler(
+                _mockTicketQueryRepository.Object,
+                _mockTicketCommandRepository.Object,
+                _mockUnitOfWork.Object);
+
+            _cancellationToken = CancellationToken.None;
+        }
+
+        [Fact]
+        public async Task Handle_WithValidRequest_ShouldDeleteTicket()
+        {
+            // Arrange
+            var ticketId = Guid.NewGuid();
+
+            var ticket = Ticket.Create(
+                id: TicketId.New(ticketId),
+                origin: "Kerman",
+                destination: "Tabriz",
+                description: "Description!",
+                travelDate: new DateTime(2026, 10, 15, 10, 15, 30, DateTimeKind.Utc),
+                price: 2000);
+
+            var command = new DeleteTicketCommand(ticketId);
+
+            _mockTicketQueryRepository.Setup(x => x.GetTicketById(ticketId, _cancellationToken))
+                .ReturnsAsync(ticket);
+
+            _mockTicketCommandRepository.Setup(x => x.DeleteTicket(ticket));
+
+            _mockUnitOfWork.Setup(x => x.SaveChangesAsync(_cancellationToken))
+                .ReturnsAsync(1);
+
+            // Act
+            var result = await _handler.Handle(command, _cancellationToken);
+
+            // Assert
+
+            result.IsSuccess.Should().BeTrue();
+            result.Error.Should().BeNull();
+            result.Value.Should().BeTrue();
+
+            _mockTicketQueryRepository.Verify(
+                x => x.GetTicketById(ticketId, _cancellationToken),
+                times: Times.Once);
+
+            _mockTicketCommandRepository.Verify(
+                x => x.DeleteTicket(It.IsAny<Ticket>()),
+                times: Times.Once);
+
+            _mockUnitOfWork.Verify(
+                x => x.SaveChangesAsync(_cancellationToken),
+                times: Times.Once);
+        }
+
+
+        [Fact]
+        public async Task Handle_ShouldFail_WhenTicketNotFound()
+        {
+            // Arrange
+            var ticketId = Guid.NewGuid();
+
+            var command = new DeleteTicketCommand(ticketId);
+
+            _mockTicketQueryRepository.Setup(x => x.GetTicketById(ticketId, _cancellationToken))
+                .ReturnsAsync((Ticket?)null);
+            // Act
+            var result = await _handler.Handle(command, _cancellationToken);
+
+            // Assert
+
+            result.IsSuccess.Should().BeFalse();
+            result.Error.Should().NotBeNull();
+            result.Error.Should().Be(Error.NotFoundError("Ticket Not Found!"));
+
+            _mockTicketQueryRepository.Verify(
+                x => x.GetTicketById(ticketId, _cancellationToken),
+                times: Times.Once);
+
+            _mockTicketCommandRepository.Verify(
+                x => x.DeleteTicket(It.IsAny<Ticket>()),
+                times: Times.Never);
+
+            _mockUnitOfWork.Verify(
+                x => x.SaveChangesAsync(_cancellationToken),
+                times: Times.Never);
+        }
+
+        [Fact]
+        public async Task Handle_ShouldFail_WhenDeleteTicketThrowException()
+        {
+            // Arrange
+            var ticketId = Guid.NewGuid();
+
+            var ticket = Ticket.Create(
+                id: TicketId.New(ticketId),
+                origin: "Kerman",
+                destination: "Tabriz",
+                description: "Description!",
+                travelDate: new DateTime(2026, 10, 15, 10, 15, 30, DateTimeKind.Utc),
+                price: 2000);
+
+            var command = new DeleteTicketCommand(ticketId);
+
+            _mockTicketQueryRepository.Setup(x => x.GetTicketById(ticketId, _cancellationToken))
+                .ReturnsAsync(ticket);
+
+            _mockTicketCommandRepository.Setup(x => x.DeleteTicket(ticket))
+                .Throws(new Exception("ef Delete exception"));
+
+
+            // Act
+            var result = await _handler.Handle(command, _cancellationToken);
+
+            // Assert
+
+            result.IsSuccess.Should().BeFalse();
+            result.Error.Should().NotBeNull();
+            result.Error.Should().Be(Error.Internal_Server(message: "ef Delete exception"));
+
+            _mockTicketQueryRepository.Verify(
+                x => x.GetTicketById(ticketId, _cancellationToken),
+                times: Times.Once);
+
+            _mockTicketCommandRepository.Verify(
+                x => x.DeleteTicket(It.IsAny<Ticket>()),
+                times: Times.Once);
+
+            _mockUnitOfWork.Verify(
+                x => x.SaveChangesAsync(_cancellationToken),
+                times: Times.Never);
+        }
+        [Fact]
+        public async Task Handle_ShouldFail_WhenSaveChangesAsyncThrowException()
+        {
+            // Arrange
+            var ticketId = Guid.NewGuid();
+
+            var ticket = Ticket.Create(
+                id: TicketId.New(ticketId),
+                origin: "Kerman",
+                destination: "Tabriz",
+                description: "Description!",
+                travelDate: new DateTime(2026, 10, 15, 10, 15, 30, DateTimeKind.Utc),
+                price: 2000);
+
+            var command = new DeleteTicketCommand(ticketId);
+
+            _mockTicketQueryRepository.Setup(x => x.GetTicketById(ticketId, _cancellationToken))
+                .ReturnsAsync(ticket);
+
+            _mockTicketCommandRepository.Setup(x => x.DeleteTicket(ticket));
+
+            _mockUnitOfWork.Setup(x => x.SaveChangesAsync(_cancellationToken))
+                .Throws(new Exception("Database exception"));
+
+            // Act
+            var result = await _handler.Handle(command, _cancellationToken);
+
+            // Assert
+
+            result.IsSuccess.Should().BeFalse();
+            result.Error.Should().NotBeNull();
+            result.Error.Should().Be(Error.Internal_Server(message: "Database exception"));
+
+            _mockTicketQueryRepository.Verify(
+                x => x.GetTicketById(ticketId, _cancellationToken),
+                times: Times.Once);
+
+            _mockTicketCommandRepository.Verify(
+                x => x.DeleteTicket(It.IsAny<Ticket>()),
+                times: Times.Once);
+
+            _mockUnitOfWork.Verify(
+                x => x.SaveChangesAsync(_cancellationToken),
+                times: Times.Once);
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.tests/Catalog.UnitTest/Features/DeleteTicket/DeleteTicketCommandValidatorTest.cs
@@ -0,0 +1,40 @@
+﻿using Catalog.API.Tickets.DeleteTicket;
+using FluentAssertions;
+
+namespace Catalog.Unit.test.Features.DeleteTicket
+{
+    public class DeleteTicketCommandValidatorTest
+    {
+        private readonly DeleteTicketCommandValidator _validator = new();
+
+        [Fact]
+        public void Validator_ShouldPass_WhenTicketIdIsValid()
+        {
+            // Arrange
+            var ticketId = Guid.NewGuid();
+
+            var command = new DeleteTicketCommand(ticketId);
+            // Act
+            var result = _validator.Validate(command);
+
+            // Assert
+            result.IsValid.Should().BeTrue();
+            result.Errors.Should().BeEmpty();
+        }
+        [Theory]
+        [InlineData("00000000-0000-0000-0000-000000000000")]
+        public void Validator_ShouldFail_WhenTicketIdIsInvalid(Guid id)
+        {
+            // Arrange
+            var ticketId = id;
+
+            var command = new DeleteTicketCommand(ticketId);
+            // Act
+            var result = _validator.Validate(command);
+
+            // Assert
+            result.IsValid.Should().BeFalse();
+            result.Errors.Should().NotBeEmpty();
+        }
+    }
+}
