bd3b537610a0ecabfaf79a5122a6adea712db73e
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-08-03 18:51:17 +0330

    feat(catalog) : Add TicketCommandRepository and interface for tickets
    - Introduced ITicketCommandRepository interface with methods for ticket CRUD operations and integration event publishing.
    - Implemented this interface in TicketCommandRepository, utilizing CatalogDbContext for data access and outbox integration.

---
+++ src/Services/Catalog/Catalog.API/Repository/ITicketCommandRepository.cs
@@ -0,0 +1,12 @@
+﻿using BuildingBlocks.Messaging;
+
+namespace Catalog.API.Repository
+{
+    public interface ITicketCommandRepository
+    {
+        public Task AddTicketAsync(Ticket ticket, CancellationToken cancellationToken = default);
+        public void UpdateTicket(Ticket ticket);
+        public void DeleteTicket(Ticket ticket);
+        public void PublishIntegrationEvent(IIntegrationEvent integrationEvent);
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Repository/TicketCommandRepository.cs
@@ -0,0 +1,29 @@
+﻿using BuildingBlocks.Infrastracture.Outbox.Extensions;
+using BuildingBlocks.Messaging;
+
+namespace Catalog.API.Repository
+{
+    public class TicketCommandRepository(CatalogDbContext context) : ITicketCommandRepository
+    {
+        public async Task AddTicketAsync(Ticket ticket, CancellationToken cancellationToken)
+        {
+
+            await context.Tickets.AddAsync(ticket, cancellationToken);
+        }
+
+        public void DeleteTicket(Ticket ticket)
+        {
+            context.Tickets.Remove(ticket);
+        }
+
+        public void UpdateTicket(Ticket ticket)
+        {
+            context.Tickets.Update(ticket);
+        }
+
+        public void PublishIntegrationEvent(IIntegrationEvent integrationEvent)
+        {
+            context.OutboxMessages.AddIntegrationEvent(integrationEvent);
+        }
+    }
+}
