2d8ccccdfe93dc9c9c8fd2dcd505bb4dbf92eb84
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-08-03 19:07:15 +0330

    refactor(catalog) : Make TicketRequestDTO properties nullable
    Changed all properties in TicketRequestDTO from non-nullable to nullable types, allowing them to accept null values. This increases flexibility for partial updates or optional fields.

--- src/Services/Catalog/Catalog.API/Common/DTOs/TicketRequestDTO.cs
+++ src/Services/Catalog/Catalog.API/Common/DTOs/TicketRequestDTO.cs
@@ -1,9 +1,9 @@
 ﻿namespace Catalog.API.Common.DTOs
 {
     public record TicketRequestDTO(
-        string Origin,
-        string Destination,
-        string Description,
-        DateTime Date,
-        decimal Price);
+        string? Origin,
+        string? Destination,
+        string? Description,
+        DateTime? Date,
+        decimal? Price);
 }
