6b6684d99c0188d1acd60a91fc2bb73e90c01ff2
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-08-16 20:35:49 +0330

    feat(Buildingblock) : add factory class that implement CustomWebApplicationFactory

---
+++ src/Services/Catalog/Catalog.tests/Catalog.IntegrationTest/Catalog.IntegrationTest/CatalogIntegrationFactory.cs
@@ -0,0 +1,10 @@
+﻿using BuildingBlocks.TestBase;
+using Catalog.API.Data;
+
+namespace Catalog.IntegrationTest
+{
+    public class CatalogIntegrationFactory : CustomWebApplicationFactory<Program, CatalogDbContext>
+    {
+
+    }
+}
