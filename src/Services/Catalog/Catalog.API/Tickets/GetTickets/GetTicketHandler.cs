52448e84258d847a9dc6ec02e7cb279a8294085c
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-08-22 18:12:03 +0330

    feat(catalog) : add pagination functionality to GetTicketEndpoint and GetTicketHandler

--- src/Services/Catalog/Catalog.API/Tickets/GetTickets/GetTicketHandler.cs
+++ src/Services/Catalog/Catalog.API/Tickets/GetTickets/GetTicketHandler.cs
@@ -1,32 +1,42 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BuildingBlocks.Pagination;
+using Catalog.API.Domain;
+using Microsoft.EntityFrameworkCore;
 
 namespace Catalog.API.Tickets.GetTickets
 {
-    public record GetTicketQuery(GetProductRequest Request) : IQuery<Result<IEnumerable<Ticket>>>;
-
+    public record GetTicketQuery(int PageNumber = 1, int PageSize = 10) : IQuery<Result<PagedResult<TicketDto>>>;
     internal sealed class GetTicketQueryHandler(ICatalogDbContext catalogDb)
-        : IQueryHandler<GetTicketQuery, Result<IEnumerable<Ticket>>>
+        : IQueryHandler<GetTicketQuery, Result<PagedResult<TicketDto>>>
     {
-        public async Task<Result<IEnumerable<Ticket>>> Handle(GetTicketQuery request, CancellationToken cancellationToken)
+        public async Task<Result<PagedResult<TicketDto>>> Handle(GetTicketQuery request, CancellationToken cancellationToken)
         {
-            var pageNumber = request.Request.PageNumber;
-            var pageSize = request.Request.PageSize;
-
-            var tickets = await catalogDb.Tickets
-                .AsNoTracking()
-                .Skip(pageNumber * pageSize)
-                .Take(pageSize)
-                .OrderBy(x => x.TravelDate)
-                .ToListAsync(cancellationToken);
-                
-
-            if (tickets is null) 
+            var pageRequest = new PageRequest
             {
-                return Result<IEnumerable<Ticket>>.Failure(
-                    Error.NotFoundError(message:"Ticket Not Found!")); 
-            }
-            return Result<IEnumerable<Ticket>>.Success(tickets);
+                PageNumber = request.PageNumber,
+                PageSize = request.PageSize,
+            };
+
+            var ticketQuery = catalogDb.Tickets
+                .AsNoTracking()
+                .OrderByDescending(x => x.TravelDate)
+                .ThenByDescending(x => x.Id);
+
+            var pagedResult = await ticketQuery.ToPagedResultAsync(
+                pageRequest: pageRequest,
+                projection: source => source.Select(ticket => new TicketDto
+                (
+                    Id: ticket.Id.Value,
+                    Origin: ticket.Origin,
+                    Destination: ticket.Destination,
+                    Description: ticket.Description,
+                    TravelDate: ticket.TravelDate,
+                    Price: ticket.Price
+                )), cancellationToken);
+
+            if (pagedResult is not null)
+                return Result<PagedResult<TicketDto>>.Success(pagedResult);
+
+            return Result<PagedResult<TicketDto>>.Failure(Error.NotFoundError("ticket not found"));
         }
     }
 }
-    
--- src/Services/Catalog/Catalog.API/Tickets/GetTickets/GetTicketsEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Tickets/GetTickets/GetTicketsEndpoint.cs
@@ -1,23 +1,30 @@
-﻿namespace Catalog.API.Tickets.GetTickets
+﻿using BuildingBlocks.Extensions;
+using BuildingBlocks.Pagination;
+
+namespace Catalog.API.Tickets.GetTickets
 {
-    public record GetProductRequest(int PageNumber = 0 , int PageSize = 10);
+    public record GetTicketRequest(PageRequest PageRequest);
     public class GetTicketsEndpoint : ICarterModule
     {
         public void AddRoutes(IEndpointRouteBuilder app)
         {
-            app.MapGet("tickets", async ([AsParameters]GetProductRequest request,ISender sender ) =>
+            app.MapGet("tickets", async (
+                HttpContext context,
+                ISender sender,
+               [FromQuery] int pageNumber,
+               [FromQuery] int pageSize) =>
             {
-                var query = await sender.Send(new GetTicketQuery(request));
-                if (query.IsSuccess)
+                var result = await sender.Send(new GetTicketQuery(pageNumber, pageSize));
+                if (result.IsSuccess)
                 {
-                    return Results.Ok(query);
+                    return Results.Ok(result);
                 }
-                return Results.Problem();
+                return result.ToHttpResult(context);
             })
               .Produces(StatusCodes.Status200OK)
               .ProducesProblem(StatusCodes.Status404NotFound)
               .ProducesProblem(StatusCodes.Status400BadRequest)
-              .WithSummary("this endpoint used for receive tickets");      
+              .WithSummary("this endpoint used for receive tickets");
         }
     }
 }
