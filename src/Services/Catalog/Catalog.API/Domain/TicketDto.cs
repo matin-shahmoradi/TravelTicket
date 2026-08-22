86ee8b196326fe114e9d8a542c4ebddf8a83fb14
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-08-22 18:09:38 +0330

    feat(catalog) : add TicketDto for the purpose of data transfer

---
+++ src/Services/Catalog/Catalog.API/Domain/TicketDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Catalog.API.Domain
+{
+    public sealed record class TicketDto
+        (
+            Guid Id,
+            string Origin,
+            string Destination,
+            string Description,
+            DateTime TravelDate,
+            decimal Price
+        );
+}
