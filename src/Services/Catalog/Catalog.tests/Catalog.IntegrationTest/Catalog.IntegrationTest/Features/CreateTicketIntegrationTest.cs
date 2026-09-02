502eb446593115bdebe19e428e130a35ca45606a
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-09-02 23:16:26 +0330

    test(catalog) : implement test for outbox

--- src/Services/Catalog/Catalog.tests/Catalog.IntegrationTest/Catalog.IntegrationTest/Features/CreateTicketIntegrationTest.cs
+++ src/Services/Catalog/Catalog.tests/Catalog.IntegrationTest/Catalog.IntegrationTest/Features/CreateTicketIntegrationTest.cs
@@ -1,4 +1,6 @@
-﻿using BuildingBlocks.TestBase;
+﻿using BuildingBlocks.Infrastracture.Outbox;
+using BuildingBlocks.Messaging.Events.CatalogEvents;
+using BuildingBlocks.TestBase;
 using Catalog.API.Data;
 using Catalog.API.Domain.ValueObjects;
 using Catalog.IntegrationTest.FakeData;
@@ -7,6 +9,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using System.Net;
 using System.Net.Http.Json;
+using System.Text.Json;
 
 namespace Catalog.IntegrationTest.Features
 {
@@ -44,13 +47,12 @@ namespace Catalog.IntegrationTest.Features
             // Arrange
             var request = FakeTicket.CreateFakeRequestDto();
 
+            // Act
             using var response = await _factory.HttpClient
                  .WithRoles("Admin")
                  .PostAsJsonAsync("/tickets", request);
 
             var ticketId = await response.Content.ReadFromJsonAsync<Guid>();
-
-            // Act
 
             var ticket = await _factory.ExecuteScopeAsync(async action =>
             {
@@ -96,6 +98,78 @@ namespace Catalog.IntegrationTest.Features
 
             // Assert
             response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+        }
+
+        [Fact]
+        public async Task CreateTicket_Should_Persist_OutboxMessage()
+        {
+            // Arrange
+            var request = FakeTicket.CreateFakeRequestDto();
+            // Act
+
+            using var response = await _factory.HttpClient
+                .WithRoles("Admin")
+                .PostAsJsonAsync("/tickets", request);
+
+            var responseBody = response.Content.ReadFromJsonAsync<Guid>();
+            response.EnsureSuccessStatusCode();
+
+            // Assert
+            using var scope = _factory.Services.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<CatalogDbContext>();
+
+            var outboxMessages = await context.OutboxMessages
+                .AsNoTracking()
+                .Where(x => x.Type.Contains(nameof(TicketCreatedIntegrationEvent)))
+                .ToListAsync();
+
+
+            var message = outboxMessages.LastOrDefault();
+            var messageType = Type.GetType(message.Type);
+
+            outboxMessages.Should().NotBeEmpty();
+
+            message.ProcessedOnUtc.Should().BeNull();
+
+            message.Error.Should().BeNull();
+
+            var deseriliziedEvent = JsonSerializer.Deserialize<TicketCreatedIntegrationEvent>(message.Content);
+
+            deseriliziedEvent.Should().NotBeNull();
+            deseriliziedEvent.Origin.Should().Contain(request.Origin);
+        }
+
+        [Fact]
+        public async Task OutboxProcessor_WhenUnprocessedMessagesExist_ShouldPublishToBroker()
+        {
+            // Arrange
+            var request = FakeTicket.CreateFakeRequestDto();
+
+            using var response = await _factory.HttpClient
+                .WithRoles("Admin")
+                .PostAsJsonAsync("/tickets", request);
+
+            response.EnsureSuccessStatusCode();
+
+            await _factory.ExecuteScopeAsync(async sp =>
+            {
+                var processor = sp.GetRequiredService<IOutboxProcessor>();
+                await processor.ProcessMessageAsync();
+            });
+
+            // Act
+            var processedMessage = await _factory.ExecuteDbContextAsync(ctx =>
+            {
+                return ctx.OutboxMessages
+                    .AsNoTracking()
+                    .Where(x => x.Type.Contains(nameof(TicketCreatedIntegrationEvent)))
+                    .FirstOrDefaultAsync();
+            });
+
+            // Assert
+            processedMessage.Should().NotBeNull();
+            processedMessage!.ProcessedOnUtc.Should().NotBeNull();
+            processedMessage.Error.Should().BeNull();
         }
     }
 }
