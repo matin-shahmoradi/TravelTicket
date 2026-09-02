d3dc6c028689a3714004e6a74edbdfaafbddb4e4
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-09-02 23:13:21 +0330

    test(Catalog) : add test database seeder

--- src/Services/Catalog/Catalog.tests/Catalog.IntegrationTest/Catalog.IntegrationTest/CatalogIntegrationFactory.cs
+++ src/Services/Catalog/Catalog.tests/Catalog.IntegrationTest/Catalog.IntegrationTest/CatalogIntegrationFactory.cs
@@ -1,10 +1,26 @@
 ﻿using BuildingBlocks.TestBase;
 using Catalog.API.Data;
+using Catalog.IntegrationTest.FakeData;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Catalog.IntegrationTest
 {
-    public class CatalogIntegrationFactory : CustomWebApplicationFactory<Program, CatalogDbContext>
+    public sealed class CatalogIntegrationFactory : CustomWebApplicationFactory<Program, CatalogDbContext>
     {
+        protected override async Task SeedDatabaseAsync()
+        {
+            await SeedTicketsAsync(20);
+        }
+
+        public async Task SeedTicketsAsync(int count, bool useNewSeed = false)
+        {
+            await using var scope = Services.CreateAsyncScope();
+            var dbContext = scope.ServiceProvider.GetRequiredService<CatalogDbContext>();
+
+            var seeder = new FakeTicket(dbContext);
+
+            await seeder.SeedTicket(count, useNewSeed);
+        }
 
     }
 }
