e2bd2df63c718775f5003dcf7766a54056202c9b
Matin shahmoradi <Matinshahmoradi01@gmail.com>
2026-09-13 18:04:39 +0330

    feat(buildingBlock) : add postgres sql open telemetry Instrumentation

--- src/Commons/BuildingBlocks/OpenTelemetry/OpenTelemetryExtensions.cs
+++ src/Commons/BuildingBlocks/OpenTelemetry/OpenTelemetryExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Npgsql;
 using OpenTelemetry.Logs;
 using OpenTelemetry.Metrics;
 using OpenTelemetry.Resources;
@@ -8,24 +9,24 @@ namespace BuildingBlocks.OpenTelemetry
 {
     public static class OpenTelemetryExtensions
     {
-        public static IServiceCollection AddTelemetry(this IServiceCollection services)
+        public static IServiceCollection AddTelemetry(this IServiceCollection services, string serviceName)
         {
             services.AddOpenTelemetry()
-                .ConfigureResource(resource => resource.AddService("TravelTicket"))
+                .ConfigureResource(resource => resource.AddService(serviceName))
                 .WithMetrics(metrics =>
                 {
                     metrics
                         .AddHttpClientInstrumentation()
-                        .AddAspNetCoreInstrumentation();
-
+                        .AddAspNetCoreInstrumentation()
+                        .AddNpgsqlInstrumentation();
                     metrics.AddOtlpExporter();
                 })
                 .WithTracing(tracing =>
                 {
                     tracing
                         .AddHttpClientInstrumentation()
-                        .AddAspNetCoreInstrumentation();
-
+                        .AddAspNetCoreInstrumentation()
+                        .AddNpgsql();
                     tracing.AddOtlpExporter();
                 })
                 .WithLogging(logging => logging.AddOtlpExporter());
